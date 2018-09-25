8c450d1a2bb5e74366414cff4ad03c7c3b55cb59
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-25 13:27:49 -0700

    reg works

--- erinc-mud/src/components/Register.js
+++ erinc-mud/src/components/Register.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
 import "../App.css";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 
 class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
+    state = { 
             username: "",
-            password: "",
+            password1: "",
+            password2: "",
             error: false,
             errorMessage: ''
-         }
+         
     }
+
+    createUser = event => {
+        event.preventDefault();
+        const user = {
+            username: this.state.username,
+            password1: this.state.password1,
+            password2: this.state.password2            
+        };
+        axios.post("http://localhost:8000/api/registration/", user).then(response => {
+            localStorage.setItem('token', response.data.key)
+            console.log(response.data.key)
+            this.props.history.push(`/login`)
+            // window.location.href = "/"
+            this.setState({
+                error: false
+            });
+        })
+            .catch(err => {
+                console.log(err)
+                this.setState({
+                    error: true,
+                    // errorMessage: err.response.data.error
+                })
+            })
+    };
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
 
 
     render() { 
@@ -28,7 +57,10 @@ class Register extends Component {
                             <input className="form-control" placeholder="Username" name='username' type="text" value={this.state.username} onChange={this.handleInputChange} />
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Password" name='password' type="password" value={this.state.password} onChange={this.handleInputChange} />
+                            <input className="form-control" placeholder="Password" name='password1' type="password" value={this.state.password1} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" placeholder="Re-enter Password" name='password2' type="password" value={this.state.password2} onChange={this.handleInputChange} />
                         </div>
                         <div className='signup-buts'>
                             <button type="submit" className="signup-button" onClick={this.createUser}>
