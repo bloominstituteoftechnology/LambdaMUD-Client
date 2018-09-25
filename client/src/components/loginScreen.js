7844eabf0304dced0bf13bd9f4eddb13836cec98
cmok4290 <cmok4290@gmail.com>
2018-09-25 16:44:19 -0400

    add post req to login endpoint

--- client/src/components/loginScreen.js
+++ client/src/components/loginScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/loginScreen.css';
+import axios from 'axios';
+
+const URL = process.env.REACT_APP_API_URL;
 
 class LoginForm extends Component {
   constructor() {
@@ -10,18 +13,40 @@ class LoginForm extends Component {
     }; 
   }
 
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    axios.post(`${URL}/login/`, user)
+      .then((res) => {
+        console.log(res);
+        this.setState({ username:'', password:'' });
+      }).catch((err) => console.log(err));
+  }
+
   render() {
     return (
       <div className="Login-Form">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             placeholder="username"
             name="username"
+            value={this.state.username}
+            onChange={this.handleInput}
           />
           <input
             placeholder="password"
             name="password"
             type="password"
+            value={this.state.password}
+            onChange={this.handleInput}
           />
           <button type="submit">Connect</button>
         </form>
