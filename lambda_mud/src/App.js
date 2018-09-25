f601a67ec09d053e777dea72c1bc478489369195
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-25 13:12:54 -0700

    Import necessary packages, copy over signin and signup components

--- lambda_mud/src/App.js
+++ lambda_mud/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: '',
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
