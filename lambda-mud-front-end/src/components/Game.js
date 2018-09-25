af1497257aa6bdbda409ca440673b2b53f8c79ee
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-25 16:31:12 -0400

    add logout

--- lambda-mud-front-end/src/components/Game.js
+++ lambda-mud-front-end/src/components/Game.js
@@ -3,9 +3,17 @@ import Authenticate from './Authenticate';
 import '../App.css';
 
 class Game extends React.Component {
+  logout = e => {
+    e.preventDefault();
+    localStorage.setItem("username", "");
+    localStorage.setItem("key", "");
+    window.location.reload();
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.logout}>Log out</button>
         GAME
       </div>
     );
