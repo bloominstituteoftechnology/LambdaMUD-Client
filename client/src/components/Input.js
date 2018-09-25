220e856fc075a43e4b7f48083fdcde0f95a5f7e3
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-25 14:55:17 -0600

    Created render of basic elements for Input.js

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Input = () => {
-    return (
-        <div className='Input'>Input</div>
-    );
+class Input extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        }
+    }
+    handleInputChange = (e) => {this.setState({[e.target.name]: e.target.value})}
+    render() { 
+        return (
+            <div className='Input'>
+                <input name='input' className='input' value={this.state.input} placeholder='Input here...' onChange={this.handleInputChange} />
+                <div className='btn'>Send</div>
+            </div>
+        );
+    }
 }
  
 export default Input;
