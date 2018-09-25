dd08292faf7dd96221edd4780fb1b80e1bbcd466
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-25 17:24:23 -0500

    game initializes displaying current player information, location information and other players in the room

--- src/Components/Adventure/Adventure.js
+++ src/Components/Adventure/Adventure.js
@@ -1,11 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
 
 class Adventure extends Component {
+  state = {
+    uuid: '',
+    name: '',
+    title: '',
+    description: '',
+    players: []
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('token')
+    axios
+      .get(process.env.REACT_APP_INIT_URL, {headers: { Authorization: token }})
+      .then(res => {
+        const uuid = res.data.uuid
+        const name = res.data.name
+        const title = res.data.title
+        const description = res.data.description
+        const players = []
+        res.data.players.forEach(player => {
+          const playerObj = {}
+          playerObj.name = player
+          players.push(playerObj)
+        })
+        this.setState({ uuid, name, title, description, players })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   render() {
     return (
-      <div>
-
+      <div className='adv-container'>
+        {this.state.uuid ? (
+          <div className='display'>
+            <div className="display-text">
+              <p>{`Player: ${this.state.name}`}</p>
+              <p>{`Location: ${this.state.title}`}</p>
+              <p>{`Description: ${this.state.description}`}</p>
+              <p>Players in room:</p>
+              <ul>
+                {this.state.players.map(player => {
+                  return <li key={Math.random()}>{player.name}</li>
+                })}                
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <p>... loading ...</p>
+        )}
       </div>
     )
   }
