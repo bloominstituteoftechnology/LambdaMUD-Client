b0ac07450a71f1dee9e8f531102afe6fd74d000f
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-25 17:48:28 -0500

    added logout button

--- src/Components/Adventure/Adventure.js
+++ src/Components/Adventure/Adventure.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import axios from 'axios'
 
+import './Adventure.css'
+
 class Adventure extends Component {
   state = {
     uuid: '',
@@ -8,6 +10,11 @@ class Adventure extends Component {
     title: '',
     description: '',
     players: []
+  }
+
+  logout() {
+    localStorage.removeItem('token')
+    window.location.pathname = '/'
   }
 
   componentDidMount() {
@@ -34,25 +41,28 @@ class Adventure extends Component {
 
   render() {
     return (
-      <div className='adv-container'>
-        {this.state.uuid ? (
-          <div className='display'>
-            <div className="display-text">
-              <p>{`Player: ${this.state.name}`}</p>
-              <p>{`Location: ${this.state.title}`}</p>
-              <p>{`Description: ${this.state.description}`}</p>
-              <p>Players in room:</p>
-              <ul>
-                {this.state.players.map(player => {
-                  return <li key={Math.random()}>{player.name}</li>
-                })}                
-              </ul>
+      <Fragment>
+        <button onClick={this.logout}>Logout</button>
+        <div className='adv-container'>
+          {this.state.uuid ? (
+            <div className='display'>
+              <div className="display-text">
+                <p>{`Player: ${this.state.name}`}</p>
+                <p>{`Location: ${this.state.title}`}</p>
+                <p>{`Description: ${this.state.description}`}</p>
+                <p>Players in room:</p>
+                <ul>
+                  {this.state.players.map(player => {
+                    return <li key={Math.random()}>{player.name}</li>
+                  })}                
+                </ul>
+              </div>
             </div>
-          </div>
-        ) : (
-          <p>... loading ...</p>
-        )}
-      </div>
+          ) : (
+            <p>... loading ...</p>
+          )}
+        </div>
+      </Fragment>
     )
   }
 }
