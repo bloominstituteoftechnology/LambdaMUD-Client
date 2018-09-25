1bba9ded1a6043edf93a35eaae472ca8b638f571
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-25 10:58:24 -0600

    Login function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
     constructor(props) {
@@ -9,13 +10,22 @@ class Login extends Component {
         }
     }
     handleInputChange = (e) => {this.setState({[e.target.name]: e.target.value})}
+    handleLogin = () => {
+        const creds = { username: this.state.username, password: this.state.password }
+        axios.post('https://lam-mud.herokuapp.com/api/login/', { creds })
+            .then(response => {
+                console.log(response.data)
+                this.props.setToken(response)
+            })
+            .catch(error => console.log(`Login: ${error}`))
+    }
     render() { 
         return (
             <div className='Login'>
                 <h1 className='title'>Login Screen</h1>
                 <input className='input' name='username' value={this.state.username} placeholder='Username' onChange={this.handleInputChange} />
                 <input className='input' name='password' value={this.state.password} placeholder='Password' onChange={this.handleInputChange} type='password' />
-                <div className='btn'>Connect</div>
+                <div className='btn' onClick={this.handleLogin}>Connect</div>
             </div>
         );
     }
