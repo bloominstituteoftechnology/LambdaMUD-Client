4ecd55e3c582cee4811cb01853b4c46599071463
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-25 14:55:59 -0600

    Created render of basic elements for Game.js

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import Console from './Console';
+import Input from './Input';
 
 class Game extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            name: '',
+            title: '',
+            description: '',
+            players: [],
+        }
     }
     render() { 
         return (
-            <div className='Game'>Game</div>
+            <div className='Game'>
+                <h1>Game View</h1>
+                <Console />
+                <Input />
+            </div>
         );
     }
 }
