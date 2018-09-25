4b5d5257eea2473362db42e6f0f06cce411aaa1a
Nicky Chen <nicky.chen0@gmail.com>
2018-09-25 10:44:17 -0700

    login page

--- adventure-game/src/components/Login.js
+++ adventure-game/src/components/Login.js
@@ -1,11 +1,46 @@
 import React from "react";
 
-const Login = () => {
-  return (
-    <div>
-      <p>Login Page</p>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      register: false
+    };
+  }
+  componentDidMount() {}
+
+  toggleRegister = event => {
+    event.preventDefault();
+    this.setState({ register: !this.state.register });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.register ? (
+          <form>
+            <h1>Create Account</h1>
+            <input placeholder="Create Account" />
+            <input placeholder="Password" />
+            <input placeholder="Password Again" />
+            <button>Connect</button>
+          </form>
+        ) : (
+          <form>
+            <h1>Login Here</h1>
+            <input placeholder="Login" />
+            <input placeholder="Password" />
+            <button>Connect</button>
+            <p>
+              No Account yet? <span onClick={this.toggleRegister}>Register here.</span>
+            </p>
+          </form>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Login;
