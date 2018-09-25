c463d1432f3a10a97842904471ea47a9766dfe3a
Simon <Simon_Freelance@hotmail.com>
2018-09-25 16:25:14 -0400

    Setup react app

---
+++ dunder-scape/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+import HomeComponent from "./components/home/HomeComponent";
+import './index';
+const AppWrapper = styled.div`
+`;
+class App extends Component {
+  render() {
+    return (
+      <AppWrapper>
+      </AppWrapper>
+    );
+  }
+}
+
+export default App;
