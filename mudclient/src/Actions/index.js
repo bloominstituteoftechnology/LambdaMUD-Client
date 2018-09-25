08627096af4ff01ef7cea66eb953a8986a8b60b0
JJ Ashcraft <jj@jjashcraft.com>
2018-09-25 18:32:31 -0400

    updated actions/reducers, added dependencies

---
+++ mudclient/src/Actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const LOGIN = 'LOGIN';
+export const LOGGING_IN = 'LOGGING_IN';
+export const REGISTER = 'REGISTER';
+export const REGISTERING = 'REGISTERING';
+
+export const userLogIn = (credentials) => {
+    return dispatch => {
+        dispatch(()=>{
+            axios
+            .post('https://mud-jjashcraft.herokuapp.com/api/login/', credentials)
+            .then(response => {
+                console.log(response);
+                localStorage.setItem(token, response.data);
+            })
+        })
+    }
+}
---
+++ mudclient/src/Reducers/index.js
@@ -0,0 +1,19 @@
+import {LOGIN, REGISTER, LOGGING_IN, REGISTERING} from './actions';
+
+const initialState = {
+loggedin: false,
+logging_in: false,
+registering: false,
+user: null,
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case LOGIN:
+        return Object.assign({}, state, {loggedin:true})
+        default:
+        return state;
+    }
+}
+
+export default rootReducer;
