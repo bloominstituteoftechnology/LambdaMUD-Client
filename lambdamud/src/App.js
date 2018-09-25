06cb81cc78d183ac4b0395ec5158b1f1c25c169f
JacobLayton <jlayjlay3@gmail.com>
2018-09-25 15:37:18 -0700

    Login component created

--- lambdamud/src/App.js
+++ lambdamud/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Register from './components/Register';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to LambdaMUD</h1>
         </header>
         <Register />
+        <Login />
       </div>
     );
   }
