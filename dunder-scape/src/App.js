a385aa38b318175723c2f1e91474902d073114b6
Simon <Simon_Freelance@hotmail.com>
2018-09-25 20:41:34 -0400

    Added more Routes and imported registerContainer

--- dunder-scape/src/App.js
+++ dunder-scape/src/App.js
@@ -4,14 +4,18 @@ import styled from "styled-components";
 import HomeComponent from "./components/home/HomeComponent";
 import './index';
 import ChoiceComponent from "./components/login-or-registration/ChoiceComponent";
+import RegisterContainer from "./components/registration/RegisterContainer";
 const AppWrapper = styled.div`
 `;
 class App extends Component {
+
+
   render() {
     return (
       <AppWrapper>
         <Route exact path = "/" component = {HomeComponent} />
-        <Route exact path = "/login-or-register" component = {ChoiceComponent} />
+        <Route exact path = "/login-or-registration" component = {ChoiceComponent} />
+        <Route exact path = "/registration" component = {RegisterContainer} />
       </AppWrapper>
     );
   }
