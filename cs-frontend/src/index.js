2db3496f755b7ebf1ae15da9b5174be40c6e5c03
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-26 13:54:40 -0700

    react router fix

--- cs-frontend/src/index.js
+++ cs-frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+// import { BrowserRouter as Router } from 'react-router';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 ReactDOM.render(
-  <Router>
+  <Router history={hashHistory}>
     <App />
   </Router>,
   document.getElementById('root')
