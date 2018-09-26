86a6eaec6297a0d3e191a7a60ae8715b1d5aeb9e
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-26 12:03:55 -0600

    Built render for Entry component

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import '../styles/Entry.css';
 
-const Entry = () => {
-    return (
-        <div className='Entry'>
-            <h1>...Test Entry...</h1>
-        </div>
-    );
+const Entry = (props) => {
+        if (typeof(props.room) === 'string') {
+            return (
+                <div className='Entry'>
+                    <p className='line'>{props.room}</p>
+                    <hr/>
+                </div>
+            )
+        }
+        else if (typeof(props.room) === 'object') {
+            return (
+                <div className='Entry'>
+                    <p className='line'>{props.room.title}</p>
+                    <p className='line'>{props.room.description}</p>
+                    {props.room.players.map(player => <p className='line' style={{ color: 'green' }} key={Math.random()}>{player} is standing here{"\n"}</p>)}
+                    <hr/>
+                </div>
+            )
+        }
 }
  
 export default Entry;
