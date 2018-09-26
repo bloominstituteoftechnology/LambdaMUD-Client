d5d41ffc9de74c041268ac4619759732c87d8696
Simon <Simon_Freelance@hotmail.com>
2018-09-26 15:29:44 -0400

    Created route to Game route

--- dunder-scape/src/App.js
+++ dunder-scape/src/App.js
@@ -6,6 +6,7 @@ import './index';
 import ChoiceComponent from "./components/login-or-registration/ChoiceComponent";
 import RegisterContainer from "./components/registration/RegisterContainer";
 import LoginContainer from "./components/login/LoginContainer";
+import GameContainer from "./components/game/GameContainer";
 const AppWrapper = styled.div`
 `;
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
         <Route exact path = "/login-or-registration" component = {ChoiceComponent} />
         <Route exact path = "/registration" component = {RegisterContainer} />
         <Route exact path = "/login" component = {LoginContainer} />
+        <Route exact path = "/game" component = {GameContainer} />
       </AppWrapper>
     );
   }
