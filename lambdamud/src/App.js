4aa2f044ddb0345e41f96657784b27a88f696e88
JacobLayton <jlayjlay3@gmail.com>
2018-09-26 11:20:09 -0700

    React Routes now working

--- lambdamud/src/App.js
+++ lambdamud/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Register from './components/Register';
-import Login from './components/Login';
-import Main from './components/Main';
+// import Register from './components/Register';
+// import Login from './components/Login';
+// import Main from './components/Main';
+import { Login, Register, Main } from './components';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -11,9 +13,9 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to LambdaMUD</h1>
         </header>
-        <Register />
-        <Login />
-        <Main />
+        <Route exact path='/' component={Main} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
       </div>
     );
   }
---
+++ lambdamud/src/components/index.js
@@ -0,0 +1,5 @@
+import Login from './Login';
+import Register from './Register';
+import Main from './Main';
+
+export { Login, Register, Main };
