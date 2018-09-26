b1621d421b0fcf677a83d2b8870b305357bd99fd
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-25 20:33:42 -0700

    init package.json

--- cs-frontend/src/App.js
+++ cs-frontend/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { n };
+
+  componentDidMount = () => {
+    //
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path="/">
+            <div className="home">//</div>
+          </Route>
+        </Switch>
       </div>
     );
   }
