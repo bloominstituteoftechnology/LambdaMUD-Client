790f4c2b8c6289119d40b1f7f10c6c2c0a91a488
cmok4290 <cmok4290@gmail.com>
2018-09-26 18:27:20 -0400

    added pusher channel and bind

--- client/src/components/mainScreen.js
+++ client/src/components/mainScreen.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import '../styles/mainScreen.css';
 import axios from 'axios';
 import Pusher from 'pusher-js';
+import { setPusherClient } from 'react-pusher';
 
 const URL = process.env.REACT_APP_API_URL;
+
+Pusher.logToConsole = true;
+
+const socket = new Pusher(`${process.env.REACT_APP_PUSHER_KEY}`, {
+  cluster: `${process.env.REACT_APP_PUSHER_CLUSTER}`,
+  forceTLS: true
+});
+
+setPusherClient(socket);
 
 class MainScreen extends Component {
   constructor() {
@@ -18,11 +28,19 @@ class MainScreen extends Component {
     axios
       .get(`${URL}/adv/init/`, {
         headers: {
-          "Authorization": "Token " + token,
+          "Authorization": `Token ${token}`,
           "Content-Type": "application/json"
         }
       })
-      .then(({data}) => console.log(data))
+      .then(({data}) => {
+        console.log(data);
+        const p_uuid = data.uuid;
+        const channel = socket.subscribe(`p-channel-${p_uuid}`);
+        channel.bind('broadcast', (data) => {
+          console.log(data);
+          this.setState({messages: data.message});
+        });
+      })
       .catch((err) => console.log(err.response)); 
   }
   
