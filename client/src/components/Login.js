8bc3c2ad3ec2337c3e7887f914081fe20d501f66
CLN <cln@Sags-MacBook-Pro.local>
2018-09-26 15:56:36 -0400

    Registration and login components working on client w heroku,.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 
 
@@ -6,34 +7,52 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            username: '',
+            password: ''
             
         }
     }
+    changeInput = e => this.setState({ [e.target.name]: e.target.value });
 
+    submitLogin = e => {
+        e.preventDefault()
+        const login = {
+            username: this.state.username,
+            password: this.state.password
+            
+        }
+        const local = 'http://127.0.0.1:8000'
+        const apiUrl = 'https://lam-mud-2.herokuapp.com'
+        axios
+            .post(`${apiUrl}/api/login`, login)
+            .then(response => {
+                console.log('logged in')
+            })
+            .catch(err => console.log(err.response))
+        this.setState({ username: '', password: '' })
+    }
     render() { 
         return (
-            <div> 
-                <input
+            <form> 
+             <input
           className="input"
-        //   value={this.state.notetitle}
-          name="notetitle"
+          value={this.state.username}
+          name="username"
           type="text"
           placeholder="username"
-        //   onChange={this.handleInputChange}
+          onChange={this.changeInput}
         />
         <input
           className="input"
-        //   value={this.state.notetitle}
-          name="notetitle"
-          type="text"
+          value={this.state.password}
+          name="password"
+          type="password"
           placeholder="password"
-        //   onChange={this.handleInputChange}
+          onChange={this.changeInput}
         />
         
-        
-                
-                
-            </div>
+        <button onClick={this.submitLogin}> Login</button>
+            </form>
         );
     }
 }
--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,46 +1,72 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
 
 class Registration extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
+            username: '',
+            password1: '',
+            password2: ''
             
         }
     }
+    changeInput = e => this.setState({ [e.target.name]: e.target.value });
 
+    submitReg = e => {
+        e.preventDefault()
+        const reg = {
+            username: this.state.username,
+            password1: this.state.password1,
+            password2: this.state.password2
+        }
+        const local = 'http://127.0.0.1:8000/api/registration/'
+        const apiUrl = 'https://lam-mud-2.herokuapp.com/api/registration/'
+        axios
+            .post(apiUrl, reg)
+            .then(response => {
+                console.log('User registered')
+            })
+            .catch(err => console.log(err.response))
+        this.setState({ username: '', password1: '', password2: '' })
+    }
     render() { 
         return (
-            <div> 
+            <form> 
                 <input
           className="input"
-        //   value={this.state.notetitle}
-          name="notetitle"
+          value={this.state.username}
+          name="username"
           type="text"
           placeholder="username"
-        //   onChange={this.handleInputChange}
+          onChange={this.changeInput}
         />
         <input
           className="input"
-        //   value={this.state.notetitle}
-          name="notetitle"
-          type="text"
+          value={this.state.password1}
+          name="password1"
+          type="password"
           placeholder="password1"
-        //   onChange={this.handleInputChange}
+          onChange={this.changeInput}
         />
         <input
           className="input"
-        //   value={this.state.notetitle}
-          name="notetitle"
-          type="text"
+          value={this.state.password2}
+          name="password2"
+          type="password"
           placeholder="password2"
-        //   onChange={this.handleInputChange}
+          onChange={this.changeInput}
         />
-        
+        <Link to='/login' onClick={this.submitReg}  >
+                    Submit
+                </Link>
+        <button onClick={this.submitReg}> SUB</button>
                 
                 
-            </div>
+            </form>
         );
     }
 }
