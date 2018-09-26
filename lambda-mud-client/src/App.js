9826da1f62f77c7943e41f352dd6fa3c169a4f47
sean chavez <seanchavez@seanchavezsMBP.home>
2018-09-25 20:31:43 -0700

    Implement authenticate functionality create empty LoginReg and Main components add axios

--- lambda-mud-client/src/App.js
+++ lambda-mud-client/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import './components/LoginReg';
+import './components/Main'
 
 class App extends Component {
+  state = { loggedIn: false }
+
+  componentDidMount() {
+    if (localStorage.getItem('lambdaMudKey')) {
+      this.setState({ loggedIn: true });
+    }
+  //   axios.get('https://mudmud.herokuapp.com/api/adv/init', {headers: {Authorization: 'Token 1e85198a887bf656c4da17ea8bc9e4e3d4eea4c7'}})
+  //     .then(response => {
+  //       console.log(response)
+  //     })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (this.state.loggedIn ? <Main /> : <LoginReg />)
   }
 }
 
