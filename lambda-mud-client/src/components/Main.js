8bcfd1c65727cb2edf9199c304fc4d5129a64e21
sean chavez <seanchavez@seanchavezsMBP.home>
2018-09-27 13:01:09 -0700

    Add 'say' functionality to command prompt

--- lambda-mud-client/src/components/Main.js
+++ lambda-mud-client/src/components/Main.js
@@ -2,34 +2,38 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Pusher from 'pusher-js';
 
-
-
+Pusher.logToConsole = true;
+const pusher = new Pusher('dac79d52d64228f70d36', {
+  cluster: 'us2',
+  encrypted: true,
+})
 
 class Main extends Component {
   state = { 
     title: '',
     description: '',
     input: '',
-    messages: ['test message'],
+    messages: [],
     players: [],
     playerName: '',
    }
+  
 
   handleInput = (e) => {
     this.setState({ input: e.target.value });
   }
 
-  handleMove = (e) => {
+  handleCommand = (e) => {
     e.preventDefault();
-    const moveDirection = { direction: this.state.input };
-    console.log(e.target.value)
-    axios.post(
-      'https://mudmud.herokuapp.com/api/adv/move',
-      moveDirection, {//{"direction":"s"}, {
-        headers: 
-         //{ Authorization: `Token ${localStorage.getItem('lambdaMudKey')}` }
-         { Authorization: `Token 1e85198a887bf656c4da17ea8bc9e4e3d4eea4c7` }
-       }
+    const command = this.state.input;
+    if (command === 'n' || command === 's' || command === 'e' || command ==='w') {
+      axios.post(
+        'https://mudmud.herokuapp.com/api/adv/move',
+        { direction: command }, 
+        {
+          headers: 
+            { Authorization: `Token ${localStorage.getItem('lambdaMudKey')}` }
+        }
     )
       .then(response => {
         console.log(response)
@@ -43,13 +47,34 @@ class Main extends Component {
       .catch(err => {
         console.log(err)
       }); 
+    } else if (this.state.input.substring(0, 4) === 'say ') {
+      axios.post(
+        'https://mudmud.herokuapp.com/api/adv/say',
+        { message: this.state.input.substring(4) },
+        {
+          headers: 
+            { Authorization: `Token ${localStorage.getItem('lambdaMudKey')}` }  
+        }
+      )
+        .then(response => {
+          console.log(response);
+          const channel = pusher.subscribe(`p-channel-${response.data.uuid}`);
+        channel.bind('broadcast', data => {
+          console.log(data)
+          this.setState({ messages: [...this.state.messages, data.message] })
+        })
+        })
+        .catch(err => {
+          console.log(err)
+        });
+    } else {
+      this.setState({ messages: [...this.state.messages, 'INVALID COMMAND'] })
+    }
+    
   }
 
   componentDidMount() {
-    const pusher = new Pusher('dac79d52d64228f70d36', {
-      cluster: 'us2',
-      encrypted: true,
-    })
+    
     axios.get(
       'https://mudmud.herokuapp.com/api/adv/init', {
        headers: 
@@ -87,8 +112,8 @@ class Main extends Component {
           })
         }
         <form>
-          <input onChange={this.handleInput} type="text" value={this.state.input}placeholder="enter direction"/>
-          <button onClick={this.handleMove}>Move</button>
+          <input onChange={this.handleInput} type="text" value={this.state.input}placeholder="enter command"/>
+          <button onClick={this.handleCommand}>Send</button>
         </form>
       </div> 
     );
