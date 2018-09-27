71d4cb0f9cd3a8fbf232d734a5299eea6133a5d3
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-27 13:11:16 -0600

    Added help function and display and created register link from root

--- src/components/App.js
+++ src/components/App.js
@@ -12,14 +12,32 @@ class App extends Component {
             token: 0
         }
     }
+    handleHelp = () => {
+        const helpBtn = document.getElementById('help')
+        const header = document.getElementById('App-header')
+        helpBtn.style.display === 'none' ? helpBtn.style.display = 'block' : helpBtn.style.display = 'none'
+        header.style.height === '1rem' ? header.style.height = '3rem' : header.style.height = '1rem'
+    }
     render() {
         return (
             <div className="App">
-                <header className="App-header">
+                <header id="App-header">
                     <h1 className="App-title">Welcome to LambdaMUD</h1>
-                    {/* Put a how-to/help div here that is hidden by default and displays on click of ? button */}
+                    <div className='btn' onClick={this.handleHelp}>Help</div>
+                    <div id='help'>
+                        <p>Navigate in any direction using N, S, E, W</p>
+                        <p>Use 'say' to speak to other users (Ex. say Hello Everyone!)</p>
+                    </div>
                 </header>
-                <Route exact path='/' render={() => <Link to='/login'>Login</Link>} />
+                <Route exact path='/' render={() => {
+                    return (
+                        <div>
+                            <Link to='/login'>Login</Link>
+                            <hr/>
+                            <Link to='/registration'>Register</Link>
+                        </div>    
+                    )
+                }} />
                 <Route path='/login' render={(props) => <Login {...props} />} />
                 <Route path='/registration' render={(props) => <Registration {...props} />} />
                 <Route path='/game' render={(props) => <Game {...props} />} />
