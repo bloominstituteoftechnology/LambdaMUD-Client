a25a8a521f7a724282620b7bb30938a13c4871b0
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-02 18:50:56 -0700

    move function

--- adv/src/components/Adventure.js
+++ adv/src/components/Adventure.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Button } from 'reactstrap';
-import './registration.css'
+import './registration.css';
+import Pusher from 'pusher-js';
+
+var pusher = new Pusher('5309daa31db0b9c7f6a6', {
+    cluster: 'us2',
+    forceTLS: true
+});
 
 class Adventure extends Component {
     constructor(props) {
@@ -11,9 +17,12 @@ class Adventure extends Component {
             name: '',
             description: '',
             players: [],
-            uuid: ''
+            uuid: '',
+            error_msg: '',
         }
     }
+
+
 
     componentDidMount() {
         const token = localStorage.getItem('token')
@@ -33,13 +42,41 @@ class Adventure extends Component {
                     players: r.data.players,
                     uuid: r.data.uuid
                 })
+                var channel = pusher.subscribe('my-channel');
+                channel.bind('my-event', function (data) {
+                    console.log('pusher data: ', JSON.stringify(data));
+                });
             })
+    }
+
+    handleMove = e => {
+        const token = localStorage.getItem('token')
+        const path = { "direction": e.target.getAttribute('value') }
+        console.log(path)
+        axios
+            .post('https://adventure-mud.herokuapp.com/api/adv/move', path, {
+                headers: {
+                    'Authorization': `Token ${token}`
+                }
+            })
+            .then(r => {
+                console.log(r)
+                this.setState({
+                    title: r.data.title,
+                    name: r.data.name,
+                    description: r.data.description,
+                    players: r.data.players,
+                    error_msg: r.data.error_msg
+                })
+            })
+
     }
 
     handleLogout = () => {
         console.log('token in handleLogout ', localStorage.getItem('token'))
         localStorage.removeItem('token');
         console.log('success,', localStorage.getItem('token'))
+        this.props.history.push('/')
     }
 
     render() {
@@ -48,9 +85,19 @@ class Adventure extends Component {
             <div className='wrapper'>
                 <div className='description'><b>Room: </b> {this.state.title}</div>
                 <div className='description'><b>Description: </b> {this.state.description}</div>
-                <div className='description'><b>Players: </b> {this.state.players.map(el => {
+                <div className='description'><b>Players online: </b> {this.state.players.map(el => {
                     return el + ' '
                 })}</div>
+                <h3>Where do you go?</h3>
+                <div className='dirArea'>
+                    <Button onClick={this.handleMove} className='dirLong' value='n'>N</Button>
+                    <Button onClick={this.handleMove} className='dirShort' value='w'>W</Button>
+                    <Button onClick={this.handleMove} className='dirShort' value='e'>E</Button>
+                    <Button onClick={this.handleMove} className='dirLong' value='s'>S</Button>
+                </div>
+                <div className='errMess'>
+                    <p>{this.state.error_msg}</p>
+                </div>
                 <br>
                 </br>
                 <Button onClick={() => this.handleLogout()} >Logout</Button>
