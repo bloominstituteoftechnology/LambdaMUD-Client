2ca33cec923ec2fb2562329d41bde25fb49bb1da
Adrian Adames <adrianadames@gmail.com>
2018-10-04 02:06:45 -0400

    Seperated CommentAndCommandInput component into two seperate components. Created first drafts of the following functions: addNewRoomSession, addComment, addCommentHandler.

--- lambdamud-adrianadames/src/App.js
+++ lambdamud-adrianadames/src/App.js
@@ -6,22 +6,76 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      userRoomSession: [
+      userRoomSessions: [
         {
           currentRoomName: '', 
           currentRoomDescription: '', 
-          playersInRoom: [{playerName:''}],
-          roomActivity: '',
-          userRoomSessionId: ''
+          playersInRoom: [],
+          roomActivity: [],
+          id: ''
         }
       ], 
-      commentAndCommandInput: '',
+      commandInput: '',
+      commentInput: ''
     }
   }
 
-  // addCommentOrCommand = (e) => {
-  //   e.preventDefault();
-  // }
+  addNewRoomSession = (e) => {
+    e.preventDefault();
+    const userRoomSessions = this.state.userRoomSessions.slice();
+
+    const userRoomSession = {
+      currentRoomName: this.state.userRoomSessions.currentRoomName,
+      currentRoomDescription: this.state.userRoomSessions.currentRoomDescription,
+      playersInRoom: this.state.userRoomSessions.playersInRoom,
+      roomActivity: this.state.userRoomSessions.roomActivity,
+      id: this.state.userRoomSessions.length+1
+    }
+
+    const userRoomSessionBlank = {
+      currentRoomName: '', 
+      currentRoomDescription: '',
+      playersInRoom: [],
+      roomActivity: [], 
+      id: ''
+    }
+
+    userRoomSessions.push(userRoomSession);
+    this.setState({userRoomSessions: userRoomSessions, userRoomSession: userRoomSessionBlank})
+  }
+
+  addComment = e => {
+    e.preventDefault();
+    const userRoomSessions = this.state.userRoomSessions.slice();
+    const userRoomSession = {
+      currentRoomName: this.state.userRoomSessions.currentRoomName,
+      currentRoomDescription: this.state.userRoomSessions.currentRoomDescription,
+      playersInRoom: this.state.userRoomSessions.playersInRoom,
+      roomActivity: this.state.userRoomSessions.roomActivity,
+      id: this.state.userRoomSessions.id
+    };
+    const comment = this.state.comment;
+
+
+    for (let i = 0; i < userRoomSessions.length; i++) {
+      if (userRoomSessions[i].userRoomSession[id] === userRoomSession[id]) {
+        userRoomSession[roomActivity] = userRoomSession[roomActivity].push(comment);
+        userRoomSessions[i] = userRoomSession;
+      }
+      else {
+        return 'Error'
+      }
+    }
+    this.setState(()=> ({userRoomSessions: userRoomSessions}));
+  }
+
+  addCommentHandler = e => {
+    console.log(e.target.value);
+
+    this.setState({
+      commentInput: [e.target.value]
+    })
+  }
 
   render() {
     return (
