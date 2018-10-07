24679d7d6986cc9456082dafe8740033be8e999b
TangledTessellations <mcevoya@oregonstate.edu>
2018-10-07 10:22:40 -0700

    Make move and say functions in Game

--- lambda_mud/src/components/gameplay/Game.js
+++ lambda_mud/src/components/gameplay/Game.js
@@ -8,92 +8,158 @@ class Game extends Component {
         this.state = {
             logged_in: this.props.logged_in,
             token: '',
-            outputText: [[]],
+            outputText: [],
             statusURL: 'https://lambda-mud-game.herokuapp.com/api/adv/init/',
-            moveURl: 'https://lambda-mud-game.herokuapp.com/api/adv/move/',
+            moveURL: 'https://lambda-mud-game.herokuapp.com/api/adv/move/',
             sayURL: 'https://lambda-mud-game.herokuapp.com/api/adv/say/',
             pusherID: '32e1f511d8c6046a4598',
         }
     }
 
+
+
     componentDidMount() {
+
         if (this.state.logged_in){
             this.startGame()
         }
     }
 
+
+
     startGame(data){
         let token = sessionStorage.getItem('token')
-        let headers = {"Authorization": `Token ${token}`}
+        let headers = { "Authorization": `Token ${token}` }
+
         try{
-            axios.get(this.state.URL, headers).then(res => {
-                if(!res.error_msg && !res.detail){
-                    console.log("Response", res)
-                    this.subscribeToPusher(res.data.uuid)
-                    this.initialize(res.data)
-                    this.setState({'token': token})
-                }
-            })
+            axios.get(this.state.URL, headers)
+                .then(res => {
+
+                    if(!res.detail){
+
+                        this.subscribeToPusher(res.data.uuid)
+                        let status = { res, textType: 'location' }
+                        this.setState({ token: token, outputText: [...this.state.outputText, status] })
+
+                    }
+                    else{
+
+                        let status = { error: res.detail, textType: 'error' }
+                        this.setState({ token: token, outputText: [...this.state.outputText, status] })
+
+                    }
+                })
         }
         catch(err){
-            return err
-        }
 
+            let status = { error: err, textType: 'error' }
+            this.setState({ outputText: [...this.state.outputText, status] })
+
+        }
     }
+
+
 
     subscribeToPusher(uuid){
         // Subscribe to Pusher broadcasts
         let pusher = new Pusher(this.state.pusherID, {
+    
             cluster: 'us2',
             forceTLS: true
+
             });
+
         let channel = pusher.subscribe(`p-channel-${uuid}`)
+
         channel.bind(`broadcast`, function(data) {
             alert(JSON.stringify(data));
         });
     }
 
 
-    actionHandler(action, args){
-        return this.callAxios(action, args).then()
 
-    }
+    move(direction){
 
-    async callAxios(action, args){
-        let token = sessionStorage.getItem('token')
-        let data
+        let header = { Authorization: `Token ${this.state.token}` }
+        direction = { direction: direction }
+
         try{
-            switch(action){
-                case 'initialize':{
-                    data = await axios({
-                        method: 'get',
-                        url: this.state.URL,
-                        headers: {"Authorization": `Token ${token}`},
-                    }).then(res => {
-                        return res
-                    })
-                    }  
-                }
-            } 
-        catch(error){
-            return error
+            axios.post(this.state.moveURL, header, direction)
+                .then(res => {
+
+                    if(!res.error_msg){
+
+                        let status = { ...res, textType: 'location' }
+                        this.setState({ outputText: [...this.state.outputText, status] })
+
+                    }
+                    else{
+
+                        let status = { error: res.error_msg, textType: 'error' }
+                        this.setState({ outputText: [...this.state.outputText, status] })
+
+                    }
+                })
         }
-        return data
-        
+        catch(err){
+
+            let status = { error: err, textType: 'error' }
+            this.setState({ outputText: [...this.state.outputText, status] })
+
+        }
     }
 
-    // handleInput(input){
-    //     loweredInput = input.toLowerCase()
-    //     switch(loweredInput){
-    //         case 'n':
 
-    //     }
-    // }
+
+
+    say(phrase){
+
+        let header = { Authorization: `Token ${this.state.token}` }
+        let message = { message: phrase }
+
+        try{
+            axios.post(this.state.sayURL, header, message)
+                .then(res => {
+
+                    if(!res.detail){
+
+                        let status = { ...res, textType: 'location' }
+                        this.setState({ outputText: [...this.state.outputText, status] })
+
+                    }
+                    else{
+
+                        let status = { error: res.detail, textType: 'error' }
+                        this.setState({ outputText: [...this.state.outputText, status] })
+
+                    }
+                })
+        }
+        catch(err){
+
+            let status = { error: err, textType: 'error' }
+            this.setState({ outputText: [...this.state.outputText, status] })
+
+        }
+    }
+
+    handleInput(input){
+
+        let commands = input.split('')
+
+        switch(commands[0].toLowerCase()){
+            case 'move':
+                
+
+        }
+    }
 
     render() {
         return (
             <div>
-                <textarea rows='10' cols='50' readOnly value={this.state.text}></textarea>
+                <div>
+                    
+                </div>
             </div>
         )
     }
