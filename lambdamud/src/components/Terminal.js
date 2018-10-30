2f293d8bd2e8e7d2323833971e17aa8ed20cce75
MichaelBoegner <cmikeb@gmail.com>
2018-10-30 14:35:21 -0400

    Wrote promise to render callback of data from users saying in room

--- lambdamud/src/components/Terminal.js
+++ lambdamud/src/components/Terminal.js
@@ -36,7 +36,7 @@ export default class Login extends Component {
                players: response.data.players,
                title: response.data.title,
                uuid: response.data.uuid,
-               terminaloutput: [`Welecome ${response.data.name}. You are in the ${response.data.title}. ${response.data.description}. Players in the room: ${response.data.players} `]
+               terminaloutput: [{message: `Welecome ${response.data.name}. You are in the ${response.data.title}. ${response.data.description}. Players in the room: ${response.data.players} `},]
            })
         })
         .catch((error) => console.log(error.response));
@@ -58,23 +58,42 @@ export default class Login extends Component {
     }
 
     render(){
-        console.log(this.state.uuid)
         const socket = new Pusher('365cf43ebb8f4a32c780', {
             cluster: 'us2',
         });
         
-        const channel = socket.subscribe(`LambdaMUD-${this.state.uuid}`);
-        console.log(channel)
+        let channel = socket.subscribe(`LambdaMUD-${this.state.uuid}`);
         
-        channel.bind('my-event', function(data) {
-            console.log('it worked!', data)
-        });
+        let getMessage = (channel) => {
+            return new Promise(
+                (resolve) => {
+                    channel.bind('my-event', function(data) {                    
+                        let newMessage = data.message;
+                        resolve(newMessage);
+                    })
+                }
+            );
+        }  
+            
+        getMessage(channel)
+            .then(newMessage => {
+                let terminaloutput = this.state.terminaloutput.slice(); 
+                terminaloutput.push({
+                    message: newMessage
+                });
+                this.setState({terminaloutput : terminaloutput});
+                })
+            .catch(error => {
+                console.log(error)
+            })    
+
+        
 
         return (
             <div>
                 <div className="terminal-output">
                     {this.state.terminaloutput.map(output => {
-                        return output;
+                        return `${output.message} \n`;
                     })}
                 </div>
 
