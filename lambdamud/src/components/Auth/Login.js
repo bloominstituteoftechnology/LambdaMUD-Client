380906beb8120f8e0f40a877df018070233f31de
AI2EJAY <JHammett1314@gmail.com>
2018-10-30 14:39:17 -0500

    React app started

---
+++ lambdamud/src/components/Auth/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  render() {
+    return <div>yo</div>;
+  }
+}
+
+export default Login;
---
+++ lambdamud/src/components/Auth/Register.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    state = {  }
+    render() { 
+        return ( <div>yo</div> );
+    }
+}
+ 
+export default Register;
