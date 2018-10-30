6eda249576a1ee2fde6af4259ab39fa1c4701248
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-30 12:01:43 -0600

    Added pusher to react app, successfully logging messages

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Pusher from 'pusher-js';
 
 class GameScreen extends Component {
   constructor(props) {
@@ -7,11 +8,12 @@ class GameScreen extends Component {
     this.state = {
       authorized: true,
       user: "",
-      userUUID: "", 
-      message: '', 
-      direction: '',
-      errors: '', 
-      messages: []
+      userUUID: "",
+      message: "",
+      direction: "",
+      errors: "",
+      messages: [], 
+      pusher: new Pusher('d9f1711efab6bafc6f93', {cluster:'us2'})
     };
   }
 
@@ -29,51 +31,67 @@ class GameScreen extends Component {
     axios
       .get("http://localhost:8000/api/adv/init", headersAuth)
       .then(response => {
-        console.log(response.data);
         this.setState({
           user: response.data.name,
-          userUUID: response.data.uuid, 
+          userUUID: response.data.uuid,
           message: `${response.data.title}: ${response.data.description}`,
-          messages: [...this.state.messages, `${response.data.title}: ${response.data.description}`]
+          messages: [
+            ...this.state.messages,
+            `${response.data.title}: ${response.data.description}`
+          ]
         });
+        const channel = this.state.pusher.subscribe("p-channel-" + response.data.uuid)
+
+        channel.bind('broadcast', response => {
+            console.log(response);
+        })
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-  onInputChange = (event) => {
-     
+  onInputChange = event => {
     this.setState({
-        direction: event.target.value
-    })
-    
-  }
+      direction: event.target.value
+    });
+  };
 
-  onSubmitHandler = (event) => {
-      const {direction, messages} = this.state
-      
-      event.preventDefault();
-      
-      const headersAuth = {
-        headers: { Authorization: `Token ${localStorage.getItem("token")}` }
-      };
+  onSubmitHandler = event => {
+    const { direction, messages } = this.state;
 
- if(direction === "e" || direction === "n" || direction === "w" || direction === "s"){
-    axios.post('http://localhost:8000/api/adv/move/', {direction}, headersAuth).then(response => {
-         console.log(response.data)
-    const {title, description} = response.data
-      this.setState({
-        message: `${title} : ${description}`, 
-        messages: [...messages, `You went ${direction}`,`${title} : ${description}`],
-        direction: ''
-         })
-      }).catch(err => {
-          console.log(err)
-      })
- }
-      
-  }
+    event.preventDefault();
+
+    const headersAuth = {
+      headers: { Authorization: `Token ${localStorage.getItem("token")}` }
+    };
+
+    if (
+      direction === "e" ||
+      direction === "n" ||
+      direction === "w" ||
+      direction === "s"
+    ) {
+      axios
+        .post("http://localhost:8000/api/adv/move/", { direction }, headersAuth)
+        .then(response => {
+          console.log(response.data);
+          const { title, description } = response.data;
+          this.setState({
+            message: `${title} : ${description}`,
+            messages: [
+              ...messages,
+              `You went ${direction}`,
+              `${title} : ${description}`
+            ],
+            direction: ""
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
 
   logOutHandler = () => {
     localStorage.clear();
@@ -86,19 +104,27 @@ class GameScreen extends Component {
         <h1>Adventure</h1>
         <h2> Welcome, {this.state.user} </h2>
         {this.state.messages.map(message => {
-            return (
-                <div  className = 'message'>
-                    <p>{message}</p>
-                </div>
-            )
-        })}
-        <form onSubmit = {this.onSubmitHandler}>
-            <div className = "form-group">
-                <input value = {this.state.direction} onChange = {this.onInputChange} type = "text" placeholder= "Enter direction" className = "form-control"/>
-                <button type = "submit" className= "btn btn-primary">Enter</button>
+          return (
+            <div className="message">
+              <p>{message}</p>
             </div>
+          );
+        })}
+        <form onSubmit={this.onSubmitHandler}>
+          <div className="form-group">
+            <input
+              value={this.state.direction}
+              onChange={this.onInputChange}
+              type="text"
+              placeholder="Enter direction"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-primary">
+              Enter
+            </button>
+          </div>
         </form>
-        <div className = "error">{this.state.errors}</div>
+        <div className="error">{this.state.errors}</div>
         <button onClick={this.logOutHandler} className="btn btn-warning">
           Log Out
         </button>
