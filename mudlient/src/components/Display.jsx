5a8f5c9bd99865dc4b87dc26fa72da278be4282e
LaikaFusion <newrad4@gmail.com>
2018-10-30 21:16:47 -0400

    split parts of page into components

---
+++ mudlient/src/components/Display.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Mudname from './Mudname'
+class Display extends Component {
+
+  render() {
+    return (
+      <div className="display">
+            <Mudname/>
+          
+            {this.props.display.map((e, i) => {
+              return (
+                <div key={i} className="displayLine">
+                  {e.time}:{e.message}
+                </div>
+              );
+            })}
+          </div>
+    );
+  }
+}
+
+export default Display;
---
+++ mudlient/src/components/InputBox.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+class InputBox extends Component {
+  render() {
+    return (
+      <div className="inputProgram">
+        <div className="pcname ">
+          {this.props.user}
+          @LambdaMUD=>
+        </div>
+        <div className="inputBoxDemos">
+          <span className="inputText">
+            {this.props.demosInput}
+            <span
+              className={
+                this.props.demosInput !== "" ? "cursor" : "cursor nochar"
+              }
+            >
+              _
+            </span>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InputBox;
