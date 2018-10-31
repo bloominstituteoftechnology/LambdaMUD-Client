38ca04feec2f2da30bcbf1a8c55f5deb334c5ba9
skennedy93 <sean47900@gmail.com>
2018-10-31 15:29:22 -0500

    still trying to make gameview work

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -3,7 +3,7 @@ import Login from './components/login';
 import './App.css';
 import Registration from './components/registration'
 import { Route } from 'react-router-dom';
-
+import GameView from './components/game'
 class App extends Component {
   constructor() {
     super();
@@ -23,6 +23,7 @@ class App extends Component {
       <div className="App">        
         <Route path = "/" component = {Login} />
         <Route path = "/registration" component = {Registration} />
+        <Route path = "/game" component = {GameView} />
       </div>
     );
   }
--- adventure/src/components/game.js
+++ adventure/src/components/game.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import React, {Component} from 'react';
 
 const url = 'https://adventuregame-app.herokuapp.com'
 
@@ -40,6 +40,50 @@ handleData = () => {
     })
   }
 
+handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value});
+}
+
+MakeMove = event => {
+    event.preventDefault();
+
+    const header = {
+        headers: {
+          authorization: `TOKEN ${localStorage.getItem('token')}`
+        }
+      };
+    
+    axios
+    .post(`${url}/api/game`, header, {direction: this.state.direction})
+    .then(response => {
+        console.log(response)
+        this.setState(response.data)
+    })
+    
+    .catch(error => console.log(error));
+};
+
+    render() {
+        return(
+        <div>
+            <div>
+                <h4>Description: {this.state.title}</h4>
+                <p>{this.state.description}</p>
+            </div>
+            <form onSubmit = {this.MakeMove}>
+                <input
+                name = 'direction'
+                value = {this.state.direction}
+                onChange = {this.handleChange}
+                placeholder = "Enter a direction"
+                />
+                <button>
+                    Press To Move
+                </button>    
+            </form>
+
+        </div>
+        )}
 
 }
 export default GameView;
