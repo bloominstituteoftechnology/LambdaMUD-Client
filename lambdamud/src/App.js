087b1d1236fa08c8a8c5fddac3d5d99d2acb71e1
MichaelBoegner <cmikeb@gmail.com>
2018-10-31 14:35:05 -0400

    Everything functional. Moving to formatting.
    Also, i learned that there is a big difference between a callback with function() in it vs an arrow function, in terms of scope

--- lambdamud/src/App.js
+++ lambdamud/src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
-import Terminal from './components/Terminal';
-import axios from 'axios';
-import './App.css';
-
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Terminal from "./components/Terminal";
+import axios from "axios";
+import "./App.css";
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      username: '',
-      password1: '',
-      password2: '',
-
-    }
+      username: "",
+      password1: "",
+      password2: "",
+    };
   }
 
 
   inputHandler = e => {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   registerHandler = e => {
@@ -29,15 +27,18 @@ export default class App extends Component {
     let newPlayer = {
       username: this.state.username,
       password1: this.state.password1,
-      password2: this.state.password2,
-    }
+      password2: this.state.password2
+    };
 
     axios
-    .post("https://lambdamudmboegner.herokuapp.com/api/registration/", newPlayer)
-    .then(response => { 
-      localStorage.setItem('Token', response.data.key);
-    })
-    .catch((error) => console.log(error.response))
+      .post(
+        "https://lambdamudmboegner.herokuapp.com/api/registration/",
+        newPlayer
+      )
+      .then(response => {
+        localStorage.setItem("Token", response.data.key);
+      })
+      .catch(error => console.log(error.response));
   };
 
   loginHandler = e => {
@@ -45,49 +46,61 @@ export default class App extends Component {
 
     let player = {
       username: this.state.username,
-      password: this.state.password1,
-    }
+      password: this.state.password1
+    };
 
     axios
-    .post("https://lambdamudmboegner.herokuapp.com/api/login/", player)
-    .then(response => { 
-      localStorage.setItem('Token', response.data.key);
-    })
-    .catch((error) => console.log(error.response))
+      .post("https://lambdamudmboegner.herokuapp.com/api/login/", player)
+      .then(response => {
+        localStorage.setItem("Token", response.data.key);
+      })
+      .catch(error => console.log(error.response));
   };
-
 
 
   render() {
     return (
       <div className="app">
         <div className="list">
-            <Route exact path="/register" render={props=> <Register 
-              {...props} 
-              inputHandler={this.inputHandler}
-              registerHandler={this.registerHandler}
-              value={this.state} 
-              />}
-            />
+          <Route
+            exact
+            path="/register"
+            render={props => (
+              <Register
+                {...props}
+                inputHandler={this.inputHandler}
+                registerHandler={this.registerHandler}
+                value={this.state}
+              />
+            )}
+          />
 
-            <Route exact path="/login" render={props=> <Login 
-              {...props} 
-              inputHandler={this.inputHandler}
-              loginHandler={this.loginHandler}
-              value={this.state} 
-              />}
-            />
+          <Route
+            exact
+            path="/login"
+            render={props => (
+              <Login
+                {...props}
+                inputHandler={this.inputHandler}
+                loginHandler={this.loginHandler}
+                value={this.state}
+              />
+            )}
+          />
 
-            <Route exact path="/terminal" render={props=> <Terminal 
-              {...props} 
-              inputHandler={this.inputHandler}
-              terminalHandler={this.terminalHandler}
-              value={this.state} 
-              />}
-            />
-
+          <Route
+            exact
+            path="/terminal"
+            render={props => (
+              <Terminal
+                inputHandler={this.inputHandler}
+                terminalHandler={this.terminalHandler}
+                value={this.state}
+              />
+            )}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
--- lambdamud/src/components/Terminal.js
+++ lambdamud/src/components/Terminal.js
@@ -1,163 +1,155 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Pusher from 'pusher-js'
-
+import React, { Component } from "react";
+import axios from "axios";
+import Pusher from "pusher-js";
 
 export default class Login extends Component {
-    constructor(){
-        super();
-        this.state = {
-            terminalinput: '',
-            terminaloutput: [],
-            description: '', 
-            name: '', 
-            players: [],
-            title: '', 
-            uuid: '',
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      terminalinput: "",
+      terminaloutput: [],
+      description: "",
+      name: "",
+      players: [],
+      title: "",
+      uuid: ""
+    };
+  }
 
-    componentDidMount() {
-
-
-        const token = localStorage.getItem('Token');
-        const reqOptions = {
-            headers: {
-                Authorization: `Token ${token}`,
-                'Content-Type': 'application/json'
-            }
-        };
-
-        axios
-        .get("https://lambdamudmboegner.herokuapp.com/api/adv/init/", reqOptions)
-        .then(response => { 
-           this.setState({
-               description: response.data.description,
-               name: response.data.name,
-               players: response.data.players,
-               title: response.data.title,
-               uuid: response.data.uuid,
-               terminaloutput: [{message: `Welecome ${response.data.name}. You are in the ${response.data.title}. ${response.data.description}. Players in the room: ${response.data.players} `},]
-           })
-        })
-        .catch((error) => console.log(error.response));
-    }
-
-
-    inputHandler = e => {
-        this.setState({[e.target.name]: e.target.value});
+  componentDidMount() {
+    const token = localStorage.getItem("Token");
+    const reqOptions = {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json"
+      }
     };
 
-    terminalHandler = e => {
-        e.preventDefault();
-        let input = this.state.terminalinput
-        const token = localStorage.getItem('Token');
-        let headers = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Token ${token}`,
+    axios
+      .get("https://lambdamudmboegner.herokuapp.com/api/adv/init/", reqOptions)
+      .then(response => {
+        this.setState({
+          description: response.data.description,
+          name: response.data.name,
+          players: response.data.players,
+          title: response.data.title,
+          uuid: response.data.uuid,
+          terminaloutput: [
+            {
+              message: `Welecome ${response.data.name}. You are in the ${
+                response.data.title
+              }. ${response.data.description}. Players in the room: ${
+                response.data.players
+              } `
             }
-        }
-        //=========== SAY COMMAND ===============//
-
-        if(input.substr(0, input.indexOf(' ')) === 'say') {
-            let message = {
-                message: input.substr(input.indexOf(' ') + 1),
-            }
-
-            axios
-            .post("https://lambdamudmboegner.herokuapp.com/api/adv/say/", message, headers)
-            .then(response => { 
-                console.log('say post SUCCESS!', response)
-                let terminaloutput = this.state.terminaloutput.slice(); 
-                terminaloutput.push({
-                    message: response.data.message
-                });
-                this.setState({terminaloutput : terminaloutput});
-            })
-            .catch((error) => console.log('say post failed',error.response));
-        } 
-        else if (input.substr(0, input.indexOf(' ')) === 'move') {
-
-            let message = {
-                direction: input.substr(input.indexOf(' ') + 1),
-            }
-
-            axios
-            .post("https://lambdamudmboegner.herokuapp.com/api/adv/move/", message, headers)
-            .then(response => { 
-                console.log('say post SUCCESS!', response)
-                let terminaloutput = this.state.terminaloutput.slice(); 
-                terminaloutput.push({
-                    message: `You are in the ${response.data.title}. ${response.data.description}. Players in the room: ${response.data.players} `
-                });
-                this.setState({terminaloutput : terminaloutput});
-            })
-            .catch((error) => console.log('say post failed',error.response));
-        }
-    }
-
-    render(){
-
-        const socket = new Pusher('365cf43ebb8f4a32c780', {
-            cluster: 'us2',
+          ]
         });
-        
+      })
+      .then(response => {
+        const socket = new Pusher("365cf43ebb8f4a32c780", {
+          cluster: "us2"
+        });
+
         let channel = socket.subscribe(`LambdaMUD-${this.state.uuid}`);
-        
-        let getMessage = (channel) => {
-            return new Promise(
-                (resolve) => {
-                    channel.bind('my-event', function(data) {                    
-                        let newMessage = data.message;
-                        resolve(newMessage);
-                    })
-                }
-            );
-        }  
-            
-        getMessage(channel)
-            .then(newMessage => {
-                let terminaloutput = this.state.terminaloutput.slice(); 
-                terminaloutput.push({
-                    message: newMessage
-                });
-                this.setState({terminaloutput : terminaloutput});
-                })
-            .catch(error => {
-                console.log(error)
-            })    
 
-        
+        channel.bind("my-event", data => {
+          this.setState({
+            terminaloutput: [...this.state.terminaloutput, data]
+          });
+        });
+      })
+      .catch(error => console.log(error.response));
+  }
 
-        return (
-            <div>
-                <div className="terminal-output">
-                    {this.state.terminaloutput.map(output => {
-                        return `${output.message} \n`;
-                    })}
-                </div>
+  inputHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-                <div className="form">
-                <div className="form-title">Terminal</div>
+  terminalHandler = e => {
+    e.preventDefault();
+    let input = this.state.terminalinput;
+    const token = localStorage.getItem("Token");
+    let headers = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`
+      }
+    };
+    //=========== SAY COMMAND ===============//
 
-                <div className="form-body">
-                    <form onSubmit={this.terminalHandler}>
-                    <input
-                        type="text"
-                        className="form-inputusername"
-                        name="terminalinput"
-                        onChange={this.inputHandler}
-                        placeholder="User input"
-                        value={this.state.terminalinput}
-                    />
-                    <button className="form-button">
-                        Send
-                    </button>
-                    </form>
-                </div>
-                </div>
-            </div>
-    );
+    if (input.substr(0, input.indexOf(" ")) === "say") {
+      let message = {
+        message: input.substr(input.indexOf(" ") + 1)
+      };
+
+      axios
+        .post(
+          "https://lambdamudmboegner.herokuapp.com/api/adv/say/",
+          message,
+          headers
+        )
+        .then(response => {
+          let terminaloutput = this.state.terminaloutput.slice();
+          terminaloutput.push({
+            message: response.data.message
+          });
+          this.setState({ terminaloutput: terminaloutput });
+        })
+        .catch(error => console.log("say post failed", error.response));
     }
+    //=========== MOVE COMMAND ===============//
+    else if (input.substr(0, input.indexOf(" ")) === "move") {
+      let message = {
+        direction: input.substr(input.indexOf(" ") + 1)
+      };
+
+      axios
+        .post(
+          "https://lambdamudmboegner.herokuapp.com/api/adv/move/",
+          message,
+          headers
+        )
+        .then(response => {
+          let terminaloutput = this.state.terminaloutput.slice();
+          terminaloutput.push({
+            message: `You are in the ${response.data.title}. ${
+              response.data.description
+            }. Players in the room: ${response.data.players} `
+          });
+          this.setState({ terminaloutput: terminaloutput });
+        })
+        .catch(error => console.log("say post failed", error.response));
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div />
+        <div className="terminal-output">
+          {this.state.terminaloutput.map(output => {
+            return `${output.message} \n`;
+          })}
+        </div>
+
+        <div className="form">
+          <div className="form-title">Terminal</div>
+
+          <div className="form-body">
+            <form onSubmit={this.terminalHandler}>
+              <input
+                type="text"
+                className="form-inputusername"
+                name="terminalinput"
+                onChange={this.inputHandler}
+                placeholder="User input"
+                value={this.state.terminalinput}
+              />
+              <button className="form-button">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
