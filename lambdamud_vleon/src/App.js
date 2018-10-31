6e30869691f0ad301d666f2e7f33c6b209b5b63e
vanceleon <vanceleon44@gmail.com>
2018-10-31 16:22:52 -0400

    install pusher dependencies created components

--- lambdamud_vleon/src/App.js
+++ lambdamud_vleon/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Link, Route} from "react-router-dom";
+import axios from "axios";
+
+
 import './App.css';
 
 
@@ -7,23 +11,47 @@ import './App.css';
 
 
 class App extends Component {
+  state = {
+    mudO: {},
+    players: "",
+    direction: "",
+    say: ""
+  }
+
+  onChange = event => {
+    this.setState({[event.target.name]: event.target.value});
+  };
+
+  componentDidMount() {
+
+  }
+
+  get 
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Change for the front end
-          </a>
-        </header>
+      <div className="mud-container">
+        <div className="header">
+          <h1>Lambda MUD</h1>
+        </div>
+        <div className="text-output">
+          {/* render the json object here */}
+        </div>
+        <form className="input-form">
+          <input type="text" name="" id=""/>
+          <div className="save-btn">
+            SEND
+          </div>
+        </form>
+        <Route
+          path="/"
+          render={ props => {
+            return (
+              <Chat/>
+                      )
+}}
+        
+        />
       </div>
     );
   }
---
+++ lambdamud_vleon/src/Components/Login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+
+    }
+
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+
+
+    }
+
+
+
+}
+
+export default Login;
