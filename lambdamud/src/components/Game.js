d0d43285f6f3495719fac7ba72cabd9f45db191b
William <kwontaewoongah@gmail.com>
2018-10-31 01:41:54 -0700

    set up interface for game

--- lambdamud/src/components/Game.js
+++ lambdamud/src/components/Game.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import axios from 'axios';
+import {withRouter} from 'react-router-dom';
+import {GameForm,GameFormHeader,GameFormMain,GameFormControls,GameFormTextSection,ActionButton,LogOutButton} from './GameComponents';
 
 class Game extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            actions:[]
+        }
+    }
     componentDidMount(){
         const token=localStorage.getItem('token');
+        localStorage.setItem('savedPage','/main');
         axios.get('https://new-school-mud.herokuapp.com/api/adv/init',{
-            'headers':{
-                'Authorization':`Token ${token}`
+            headers:{
+                Authorization:`Token ${token}`
                 }
             })
-            .then(res=>console.log(res))
+            .then(res=>{
+                const actions=this.state.actions.slice();
+                actions.push(res.data);
+                this.setState({actions:actions},()=>console.log(this.state.actions));
+            })
             .catch(err=>console.log(err))
+    }
+    logout=()=>{
+        localStorage.removeItem('token');
+        this.props.history.push('/login');
     }
     render(){
         return(
-            <div></div>
+            <GameForm>
+                <GameFormHeader>Adventure</GameFormHeader>
+                <GameFormMain>
+                    <GameFormTextSection>
+                    </GameFormTextSection>
+                    <GameFormControls>
+                        <input/>
+                        <ActionButton>Send</ActionButton>
+                        <LogOutButton onClick={this.logout}>Log Out</LogOutButton>
+                    </GameFormControls>
+                </GameFormMain>
+            </GameForm>
         )
     }
 }
-export default Game;
+export default withRouter(Game);
---
+++ lambdamud/src/components/GameComponents.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const GameForm=styled.form`
+    max-width:600px;
+    width:100%;
+    height:auto;
+    
+`
+export const GameFormHeader=styled.div`
+    background-color:#003399;
+    height:40px;
+    color:#FFF;
+    font-size:16px;
+    display:flex;
+    justify-content:flex-start;
+    align-items:center;
+    padding-left:30px;
+    border-radius:3px;
+`
+export const GameFormMain=styled.div`
+    background-color:#FFF;
+    height:300px;
+    width:596px;
+    border:2px solid #003399;
+    border-radius:3px;
+`
+export const GameFormControls=styled.div`
+    background-color:#d3d3d3;
+    height:50px;
+    display:flex;
+    justify-content: space-evenly;
+    align-items:center;
+    input {
+        width:65%;
+        height:20px;
+        border-radius:3px;
+        outline:none;
+        border-color:transparent;
+    }
+`
+export const GameFormTextSection=styled.div`
+    height:250px;
+`
+export const ActionButton=styled.button`
+    background-color:#ffbb33;
+    color:#FFF;
+    font-size:14px;
+    outline:none;
+    border-color:transparent;
+    width:70px;
+    height:25px;
+`
+export const LogOutButton=styled.button`
+    background-color:#cc443d;
+    color:#FFF;
+    font-size:14px;
+    outline:none;
+    border-color:transparent;
+    width:70px;
+    height:25px;
+`
+
