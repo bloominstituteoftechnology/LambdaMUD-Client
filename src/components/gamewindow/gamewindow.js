6b973c1f16122b902b11c51091ac9441ec9cc9b4
adamcglee <adamcglee@gmail.com>
2018-10-31 14:00:29 -0700

    say and move functions are working. Add some basic html elements to display info in the game window

--- src/components/gamewindow/gamewindow.js
+++ src/components/gamewindow/gamewindow.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { splitStringTransformer } from 'common-tags';
+import { splitStringTransformer } from "common-tags";
 import axios from "axios";
 
 class GameWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      player: {},
+      player: {
+        description: "",
+        name: "",
+        players: [],
+        title: "",
+        uuid: "",
+      },
       message: ""
     };
   }
@@ -15,6 +21,7 @@ class GameWindow extends Component {
     const headersAuthorization = {
       headers: { Authorization: `Token ${localStorage.getItem("key")}` }
     };
+    console.log(localStorage.getItem("key"))
     axios
       .get(
         "https://lambdamud-alee.herokuapp.com/api/adv/init",
@@ -25,6 +32,7 @@ class GameWindow extends Component {
         console.log(this.state.player);
       })
       .catch(err => console.log(err));
+
   }
 
   handleTextChange = event => {
@@ -41,34 +49,52 @@ class GameWindow extends Component {
     const headersAuthorization = {
       headers: { Authorization: `Token ${localStorage.getItem("key")}` }
     };
-    const messageArr = this.state.message.split(" ");
-    console.log(messageArr[0])
-    if(messageArr[0] === "/s"){
-        axios
-        .post("https://lambdamud-alee.herokuapp.com/api/adv/say", this.state.message, headersAuthorization)
+    console.log(localStorage.getItem("key"))
+    const messageArr = this.state.message.toLowerCase().split(" ");
+    console.log(messageArr[0]);
+    if (messageArr[0] === "/s") {
+      messageArr.shift();
+      console.log(messageArr);
+      let mymessage = messageArr.join(" ");
+      console.log(mymessage);
+      axios
+        .post(
+          "https://lambdamud-alee.herokuapp.com/api/adv/say/",
+          {message: mymessage},
+          headersAuthorization
+        )
         .then(response => {
-            this.setState({ 
-                player: response.data,
-                message: ""
+          this.setState({
+            message: response.data.message
+          });
+          console.log(this.state.message);
+          this.setState({
+            message: ""
+          });
+        })
+        .catch(err => console.log(err));      
+    } else if (messageArr[0] === "go") {
+      if (messageArr[1] === "n" || "s" || "e" || "w") {
+        axios
+          .post(
+            "https://lambdamud-alee.herokuapp.com/api/adv/move/",
+            {direction: messageArr[1]},
+            headersAuthorization
+          )
+          .then(response => {
+            this.setState({
+              player: response.data,
+              message: response.data.message
             });
             console.log(this.state.player);
-        })
-        .catch(err => console.log(err));
-    }
-    else if(messageArr[0] === "n" || "s" || "e" || "w"){
-        axios
-        .post("https://lambdamud-alee.herokuapp.com/api/adv/move", messageArr[0], headersAuthorization)
-        .then(response => {
-            this.setState({ 
-                player: response.data,
-                message: ""
+            this.setState({
+              message: ""
             });
-            console.log(this.state.player);
-        })
-        .catch(err => console.log(err));
-    }
-    else{
-        console.log("Not a valid command!")
+          })
+          .catch(err => console.log(err));
+      }      
+    } else {
+      console.log("Not a valid command!");
     }
   };
 
@@ -78,26 +104,30 @@ class GameWindow extends Component {
         <h1> Yay LambdaMud!</h1>
         <button onClick={this.handleLogout}>Logout</button>
         <div>
-          <ul>
-            {/* {this.props.messages.map(message => {
-              return (
-                <li key={message.id}>
-                  <div>{message.senderId}</div>
-                  <div>{message.text}</div>
-                </li>
-              );
-            })} */}
-          </ul>
+          <h2>Current Room</h2>
+          <h3>{this.state.player.title}</h3>
         </div>
+        <hr/>
+        <div>
+          <h3>Room Description</h3>
+          <h4>{this.state.player.description}</h4>
+        </div>
+        <hr/>
+        <div>
+          <h3>Players in the Room</h3>
+          {this.state.player.players.length != 0 ? <h4>{this.state.player.players}</h4> : <h4>No One</h4>}
+        </div>
+        <hr/>
         <div>
           <form onSubmit={this.submitCommand}>
             <input
               type="text"
               placeholder="Enter Command"
               onChange={this.handleTextChange}
-              value={this.state.message}
+              value={this.state.message || ''}
               name="message"
             />
+            <button>Submit</button>
           </form>
         </div>
       </div>
