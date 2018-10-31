1ad8b2341bcb31a04101e5eb6ca8b46a13c4dff8
William <kwontaewoongah@gmail.com>
2018-10-30 21:27:14 -0700

    getting response from api call to init

--- lambdamud/src/components/Game.js
+++ lambdamud/src/components/Game.js
@@ -2,11 +2,15 @@ import React from 'react';
 import axios from 'axios';
 
 class Game extends React.Component{
-    constructor(){
-        super();
-        this.state={
-
-        }
+    componentDidMount(){
+        const token=localStorage.getItem('token');
+        axios.get('https://new-school-mud.herokuapp.com/api/adv/init',{
+            'headers':{
+                'Authorization':`Token ${token}`
+                }
+            })
+            .then(res=>console.log(res))
+            .catch(err=>console.log(err))
     }
     render(){
         return(
