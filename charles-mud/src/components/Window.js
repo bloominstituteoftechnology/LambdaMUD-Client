bd68b8c8a269924fd683dc59c1fa8958ccde2507
Charles Cortinas <crcortinas@gmail.com>
2018-11-01 14:48:02 -0500

    pusher
    pusher functionality

--- charles-mud/src/components/Window.js
+++ charles-mud/src/components/Window.js
@@ -1,13 +1,22 @@
 import React from "react";
 import InputComponent from "./InputComponent";
 import axios from 'axios';
+import Pusher from 'pusher-js';
 
 class Window extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       url: 'https://charles-mud.herokuapp.com',
-      headers: {'Authorization': 'Token ' + localStorage.getItem('key')}
+      headers: {'Authorization': 'Token ' + localStorage.getItem('key')},
+      description: '',
+      name: '',
+      players: [],
+      title: '',
+      uuid: '',
+      channel: null,
+      loading: true
     }
   }
 
@@ -16,20 +25,54 @@ class Window extends React.Component {
     const auth = 'Token '.concat(localStorage.getItem('key'));
     axios.get(init, { headers: { Authorization: auth } })
       .then((res) => {
-        console.log('res', res);
+        console.log('axios call');
+        const pusher = new Pusher("4a0a79cd8884db0caca6", { cluster: 'us2' });
+        console.log('data', res.data);
+        var channel = pusher.subscribe(`p-channel-${res.data.uuid}`);
+        this.setState({
+          description: res.data.description,
+          name: res.data.name,
+          players: res.data.players,
+          title: res.data.title,
+          uuid: res.data.uuid,
+          channel: channel
+        });
+      })
+      .then((res) => {
+        console.log('set loading to false');
+        this.setState({ loading: false });
       })
       .catch((err) => {
-        console.log(err);
+        console.log('catch err', err);
       })
   }
 
   render() {
-    return (
-      <div className="textwindow">
-        <p>window stuff</p>
-        <InputComponent />
-      </div>
-    );
+    while (this.state.loading) {
+      return (
+        <div className='loading'>
+          <h1>Loading...</h1>
+        </div>
+      )
+    }
+    while(!this.state.loading) {
+      try{
+        if(this.state.channel){
+          this.state.channel.bind('broadcast', function(data) {
+            alert('An event was triggered with message: ' + data.message);
+          });
+        }
+      }
+      catch(err){
+        console.log('error: ', err);
+      }
+      return (
+        <div className="textwindow">
+          <p>window stuff</p>
+          <InputComponent />
+        </div>
+      );
+    }
   }
 }
 
