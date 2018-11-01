fa978ca89570971ca5d498676934f5b72537891c
Charles Cortinas <crcortinas@gmail.com>
2018-11-01 18:53:59 -0500

    Implemented things

--- charles-mud/src/components/InputComponent.js
+++ charles-mud/src/components/InputComponent.js
@@ -1,11 +1,38 @@
 import React from "react";
 
-const InputComponent = props => {
-  return (
-    <div>
-      <p>input stuff</p>
-    </div>
-  );
-};
+class InputComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      command: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    let parts = this.state.command.split(' ');
+    let comm = parts.shift();
+    let content = parts.join(' ');
+    this.props.callApi(comm, content);
+    this.setState({ command: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <input name='command' type='text' onChange={this.changeHandler} placeholder="Enter a command" value={this.state.command} />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default InputComponent;
--- charles-mud/src/components/Window.js
+++ charles-mud/src/components/Window.js
@@ -7,23 +7,31 @@ class Window extends React.Component {
 
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
+    this.callApi = this.callApi.bind(this);
+    this.createPost = this.createPost.bind(this);
     this.state = {
-      url: 'https://charles-mud.herokuapp.com',
-      headers: {'Authorization': 'Token ' + localStorage.getItem('key')},
+      url: 'https://charles-mud.herokuapp.com/api/adv/',
+      token: 'Token ' + localStorage.getItem('key'),
       description: '',
       name: '',
       players: [],
       title: '',
       uuid: '',
       channel: null,
-      loading: true
+      loading: true,
+      content: []
     }
   }
 
   componentDidMount() {
-    const init = this.state.url + '/api/adv/init'
-    const auth = 'Token '.concat(localStorage.getItem('key'));
-    axios.get(init, { headers: { Authorization: auth } })
+    const init = this.state.url + 'init'
+    //const auth = 'Token '.concat(localStorage.getItem('key'));
+    let content = [];
+    for(let i=0;i<17;i++){
+      content.push('\n');
+    }
+    axios.get(init, { headers: { Authorization: this.state.token } })
       .then((res) => {
         console.log('axios call');
         const pusher = new Pusher("4a0a79cd8884db0caca6", { cluster: 'us2' });
@@ -39,37 +47,127 @@ class Window extends React.Component {
         });
       })
       .then((res) => {
-        console.log('set loading to false');
-        this.setState({ loading: false });
+        this.setState({ loading: false, content: content });
+        this.createPost('description');
+        this.state.channel.bind('broadcast', (data) => {
+          console.log('data', data);
+          this.createPost('say', data.message);
+        });
+      })
+      .then(() => {
+        this.myRef.current.scrollTo(0, 999999999999999999999);
       })
       .catch((err) => {
         console.log('catch err', err);
       })
   }
 
+  componentDidUpdate() {
+    console.log('current myRef', this.myRef.current);
+    if(this.myRef.current) {this.myRef.current.scrollTo(0, 999999999999999999999);}
+  }
+
+  createPost(type, content=null) {
+    let newPost;
+    switch (type) {
+      case 'description':
+        newPost = (
+            <div>
+              <p>{this.state.title}</p>
+              <p>{this.state.description}</p>
+              <p>Players: {this.state.players.join(', ')}</p>
+            </div>
+          )
+        break;
+      case 'say':
+        console.log('content', content);
+        newPost = (
+          <div>
+            <p>{content}</p>
+          </div>
+        )
+        break;
+      default:
+        break;
+    }
+    let posts = this.state.content;
+    posts.push(newPost);
+    this.setState({ content: posts });
+  }
+
+  callApi(comm, content=null) {
+    let url, payload, postType;
+    content.toLowerCase();
+    switch (comm) {
+      case 'move':
+        postType = 'description';
+        url = this.state.url + 'move';
+        let dir = '';
+        switch (content){
+          case 'n':
+          case 'north':
+            dir = 'n';
+            break;
+          case 'e':
+          case 'east':
+            dir = 'e';
+            break;
+          case 's':
+          case 'south':
+            dir = 's';
+            break;
+          case 'w':
+          case 'west':
+            dir = 'w';
+            break;
+          default:
+            break;
+        }
+        payload = {"direction":dir};
+        break;
+      case 'say':
+        url = this.state.url + 'say';
+        payload = {'message':content};
+        postType = 'say';
+        break;
+      default:
+        break;
+    }
+    axios
+      .post(url, payload, { headers: { Authorization: this.state.token } })
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          description: res.data.description,
+          name: res.data.name,
+          players: res.data.players,
+          title: res.data.title
+        });
+      })
+      .then(() => {
+        this.createPost(postType);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   render() {
-    while (this.state.loading) {
+    if (this.state.loading) {
       return (
         <div className='loading'>
           <h1>Loading...</h1>
         </div>
       )
-    }
-    while(!this.state.loading) {
-      try{
-        if(this.state.channel){
-          this.state.channel.bind('broadcast', function(data) {
-            alert('An event was triggered with message: ' + data.message);
-          });
-        }
-      }
-      catch(err){
-        console.log('error: ', err);
-      }
+    } else {
       return (
-        <div className="textwindow">
-          <p>window stuff</p>
-          <InputComponent />
+        <div>
+          <div className="textwindow" ref={this.myRef} >
+            {this.state.content.map((post) => {
+              return post;
+            })}
+          </div>
+          <InputComponent callApi={this.callApi} />
         </div>
       );
     }
