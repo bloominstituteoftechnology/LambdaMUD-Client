916887071a6a9ab552eb2d4ca81ca810cbf04c8b
Adrian Adames <adrianadames@gmail.com>
2018-11-03 01:42:41 -0400

    Started to work on CommandInput component, but I'm putting it on hold for now to work on CommentInput component functionality.

---
+++ lambdamud-adrianadames/src/Components/CommandInput.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// const CommandInput = props => {
+//     return(
+//         <form onSubmit = {props.addComment()}>
+//             <div>
+//                 <input 
+//                     type = 'text'
+//                     name = 'Enter comment'
+//                     value = {props.commentInput}
+//                     onChange = {props.addCommentHandler}
+//                 />
+//             </div>
+//             <div>
+//                 <button type = 'submit'>Enter command</button>
+//             </div>
+//         </form>
+//     )
+// }
+
+// export default CommandInput; 
