479c180a8d48c3c7b8240cce84e911605359185c
Lisa Canini <three_nineteen@yahoo.com>
2018-11-29 20:18:44 -0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
 import Window from "./Components/Window";
+import { Route } from "react-router-dom";
+import "./style.css";
 
 const App = () => (
-  <div className="App">
-    <SignUp />
-    <Login />
-    <Window />
+  <div className="app">
+    <Route path="/api/registration" component={ SignUp } />
+    <Route path="/api/login" component={ Login } />
+    <Route path="api/adv/init" component={ Window } />
   </div>
 );
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-render(<App />, document.getElementById('root'));
+import App from "./App";
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("root")
+);
