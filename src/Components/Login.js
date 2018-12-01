d89454c52ec93cbe3a88424cb812dc717b379795
Lisa Canini <three_nineteen@yahoo.com>
2018-12-01 10:54:40 -0800

    passing token from login to window

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import "../styles/forms.css";
-import {Link} from "react-router-dom";
 import axios from 'axios';
 
 class Login extends Component {
@@ -21,7 +20,9 @@ class Login extends Component {
     axios
         .post("https://lisacee-mud.herokuapp.com/api/login", user)
         .then(res => {
-            console.log('RESPONSE', res)
+            localStorage.setItem('Token', res.data.key);
+            console.log('RESPONSE', res.data.key)
+            this.props.history.push('/api/adv/init');
         })
         .catch(error => {
             console.log(error.response.data)
@@ -33,9 +34,7 @@ class Login extends Component {
               <h3>Login</h3>
               <input type="text" name="username" placeholder="username" onChange={this.handleChange} />
               <input type="text" name="password" placeholder="password" onChange={this.handleChange} />
-              <Link to={"/api/adv/init"}>
                 <button type="submit">Login</button>
-              </Link>
             </form>
           </div>;
     }
--- src/Components/Window.js
+++ src/Components/Window.js
@@ -1,56 +1,82 @@
-import React from "react";
+import React, {Component} from "react";
 import { Container, Row, Col } from "reactstrap";
+import axios from 'axios';
+const players = ["Lola", "Lisa", "Jakobi", "Baxter"];
+const messages = ["Lisa says Hello", "Jakobi says I Win!"];
 
-const Window = () => {
-    const players = ["Lola", "Lisa", "Jakobi", "Baxter"];
-    const messages = ["Lisa says Hello", "Jakobi says I Win!"];
-    return (
-        <div className="window">
+class Window extends Component {
+   state = {
+            username: "",
+            uuid: "",
+            token: "",
+            room: "",
+            description: "",
+            players: [],
+            messages: []
+
+        }
+    componentDidMount() {
+    const token = {headers: {
+        Authorization: `Token ${localStorage.getItem('Token')}` 
+    }}
+    // console.log('TOKEN', token)
+    axios
+    .get('https://lisacee-mud.herokuapp.com/api/adv/init', token)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(error => {
+        console.log(error.response.data)
+    })
+    }
+    
+
+    render() {
+        // console.log(this.state)
+        return <div className="window">
             <Container>
-                <Row>
-                    <div id="room">
-                        <h4>Foyer</h4>
-                        <p>
-                            Dim light filters in from the south. Dusty passages run north and
-                            east.
-            </p>
-                        <p>*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*</p>
-                    </div>
-                </Row>
-                <Row>
-                    <div>
-                        <h4>User Input</h4>
-                        <input />
-                        <button>Send</button>
-                    </div>
-                </Row>
-                <Row>
-                    <Col sm="5">
-                        <div>
-                            <h4>Players</h4>
-                            { players.map((name, id) => (
-                                <li key={id}>*{ name }</li>
-                            )) }
-                        </div>
-                    </Col>
-                    <Col sm="1">
-                    * 
-                    *
-                    *
-                    *
-                    *</Col>
-                    <Col sm="6">
-                        <div>
-                            <h3>Messages</h3>
-                            { messages.map((message, id )=> (
-                                <li key={id}>*{ message }</li>
-                            )) }
-                        </div>
-                    </Col>
-                </Row>
+              <Row>
+
+                <div id="room">
+                  <h4>Foyer</h4>
+                  <p>
+                    Dim light filters in from the south. Dusty passages
+                    run north and east.
+                  </p>
+                  <p>*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*</p>
+                </div>
+              </Row>
+              <Row>
+                <div>
+                  <h4>User Input</h4>
+                  <input />
+                  <button>Send</button>
+                </div>
+              </Row>
+              <Row>
+                <Col sm="5">
+                  <div>
+                    <h4>Players</h4>
+
+                    {players.map((name, id) => (
+                      <li key={id}>*{name}</li>
+                    ))}
+                  </div>
+                </Col>
+                <Col sm="1">* * * * *</Col>
+                <Col sm="6">
+                  <div>
+                    <h3>Messages</h3>
+                    {messages.map((message, id) => (
+                      <li key={id}>*{message}</li>
+                    ))}
+                  </div>
+                </Col>
+              </Row>
             </Container>
-        </div>
-    );
+          </div>;
+    }
+    
 };
 
 export default Window;
