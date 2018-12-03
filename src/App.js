2e8205b9a0a198023c42528601ce477b8c011252
darthMinimus <irishseanvaldivia@gmail.com>
2018-12-02 21:05:18 -0600

    Still working on the views.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import HomeScreen from './HomeScreen.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Sean's App</div>
+        <HomeScreen/>
       </div>
     );
   }
---
+++ src/HomeScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default HomeScreen;
+
+//<Button color="primary">Signup</Button>
+//<Button color="primary">Login</Button>
+            
