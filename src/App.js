9d2dce34117bd920250ac124541ec1d75992c4b2
darthMinimus <irishseanvaldivia@gmail.com>
2018-12-05 20:53:02 -0600

    View not changing on user click...

--- src/App.js
+++ src/App.js
@@ -6,12 +6,40 @@ import Register from './Register';
 import HomeScreen from './HomeScreen';
 
 class App extends Component {
+  
+  state = {
+    login: false,
+    register: false,
+  }
+
+  handleClick = (e) => {
+    const id = e.target.id;
+    this.setState({
+      [id]: true
+    })
+  }
+
   render() {
     
+    const initialScreen = 
+    <div>
+      <div style={{
+          border: "1px solid grey",
+          height: "20vh",
+          width: "30vw",
+          display: "flex",
+          justifyContent: "center"
+    }}>
+      The Super Adventure Game
+      </div>
+      <button id="login" onClick={this.handlClick}>Login</button>
+      <button>Signup</button>
+    </div>;
+
     return ( 
       <div className="App">
-        <HomeScreen/>
-      </div>
+        {this.state.login ? LoginForm : initialScreen }
+    </div>
     );
   }
 }
