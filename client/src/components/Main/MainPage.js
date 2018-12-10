838bfb87a7287dff5a8747275c26f75361ca15a0
Nicocchi <nicocchilover@gmail.com>
2018-12-10 17:28:05 -0500

    basic game view

--- client/src/components/Main/MainPage.js
+++ client/src/components/Main/MainPage.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import Styled from 'styled-components';
+import Room from './Room';
+
+const Wrapper = Styled.div`
+    margin: 100px auto;
+    width: 600px;
+    height: 400px;
+    border: 1px solid black;
+`
+const TextArea = Styled.div`
+    margin: 20px auto;
+    width: 500px;
+    height: 250px;
+    border: 1px solid black;
+`
+
+const UserInput = Styled.div`
+    margin: 20px auto;
+    width: 500px;
+    height: 50px;
+`
 
 class MainPage extends Component {
+
+    handleButton = e => {
+        e.preventDefault();
+        this.props.handleSubmit();
+    }
+
     render() {
         return (
-            <div>
+            <Wrapper>
                 <h1>Main Page</h1>
-            </div>
+                <form onSubmit={this.handleButton} >
+                    <TextArea>
+                        <Room title={this.props.title} description={this.props.description}/>
+                    </TextArea>
+                    <UserInput>
+                        <input type="text"
+                               name="command"
+                               defaultValue={this.props.command}
+                               placeholder="User input"
+                               onChange={this.props.handleChange}
+                        />
+                        <button onClick={this.handleButton}>Send</button>
+                    </UserInput>
+
+                </form>
+            </Wrapper>
         )
     }
 };
--- client/src/views/MainView.js
+++ client/src/views/MainView.js
@@ -2,8 +2,47 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 
 import MainPage from '../components/Main/MainPage';
+import Room from '../components/Main/Room';
+import axios from "axios";
 
 class MainView extends Component {
+    state = {
+        command: '',
+        title: '',
+        description: '',
+    }
+
+    handleChange = e => {
+        this.setState({
+            ...this.state.command,
+            command: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        let token = localStorage.getItem('key');
+        console.log(token);
+
+        let config = {
+            headers: {
+                Authorization: `Token ${token}`
+            }
+        }
+
+        const endpoint = 'http://localhost:8000/api/adv/move';
+
+        const command = { "direction": `${this.state.command}`};
+
+        axios.post(endpoint, command, config).then(res => {
+            console.log(res);
+            this.setState({
+                title: res.data.title,
+                description: res.data.description
+            });
+        }).catch(err => {
+            console.log('MOVE_ERROR', err)
+        })
+    }
 
     render() {
         let isLogged = false;
@@ -14,7 +53,14 @@ class MainView extends Component {
         }
         return (
             <Fragment>
-                {!isLogged ? 'You must login' : <MainPage {...this.props} /> }
+                {!isLogged ? 'You must login' : <MainPage
+                    {...this.props}
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                    command={this.state.command}
+                    title={this.state.title}
+                    description={this.state.description}
+                /> }
             </Fragment>
         )
     }
