22237fdd01e3cf0cedc4b899c149ed8d82df0408
Csillag61 <csillagok@gmail.com>
2018-12-11 10:27:08 -0500

    nav and register

---
+++ adventure/src/components/Register.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Form from '.../styles/form';
+
+class Register extends Component {
+    state = {
+        username: '',
+        password1: '',
+        password2: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.registerUser(this.state, this.props.history);
+    }
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        value={this.state.username}
+                        name="username"
+                        onChange={this.handleChange}
+                        placeholder="Username"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={this.state.password1}
+                        name="password1"
+                        onChange={this.handleChange}
+                        placeholder="Password"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={this.state.password2}
+                        name="password2"
+                        onChange={this.handleChange}
+                        placeholder="Confirm Password"
+                        required
+                    />
+                    <button type="submit">Register</button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default Register;
