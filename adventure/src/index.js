404fa14913fbb5319ce1ad19b269c673bf8a5504
Csillag61 <csillagok@gmail.com>
2018-12-11 10:36:25 -0500

    styled nav

--- adventure/src/index.js
+++ adventure/src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import App from "./App";
 import { rootReducer } from "./reducers";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
--- adventure/src/styles/nav.js
+++ adventure/src/styles/nav.js
@@ -1,12 +1,33 @@
 import styled from 'styled-components';
 
 const StyledNav = styled.div`
-
-
-
-
-
-
-
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: #636e72;
+  margin-bottom: 2rem;
+  height: 6rem;
+  a {
+    padding: 1rem;
+    font-size: 2rem;
+    color: white;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    background-color: inherit;
+    color: white;
+    border: none;
+    font-size: 2rem;
+    cursor: pointer;
+    font-family: inherit;
+    padding: 1rem;
+    &:focus {
+      outline: none;
+    }
+  }
+`;
 
 export default StyledNav;
