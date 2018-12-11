41c33e97715a9f7299f37d5812b9bbc5a644ba1b
verydecent <wnjhwng@gmail.com>
2018-12-11 13:53:11 -0800

    setting up login/register

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,82 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <LoginBox />
+            <RegisterBox />
       </div>
     );
   }
+}
+
+
+class LoginBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            test: '1'
+        }
+    }
+
+    submitlogin(e) {
+    }
+
+    render() {
+        return (
+            <div className="hedge-container">
+                <div className="register-header">
+                    Login
+                </div>
+                <div className="login-container">
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input className="login-input" type="text" name="username" placeholder="Username"></input>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input className="login-input" type="password" name="password" placeholder="Password"></input>
+                    </div>
+
+                    <button className="login-button" type="button" onClick={this.submitlogin.bind(this)}>Login</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+class RegisterBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            test: '1'
+        }
+    }
+
+    submitregister(e) {
+    }
+
+    render() {
+        return (
+            <div className="hedge-container">
+                <div className="register-header">
+                    Register
+                </div>
+                <div className="register-container">
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input className="register-input" type="text" name="username" placeholder="Username"></input>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input className="register-input" type="password" name="password" placeholder="Password"></input>
+                    </div>
+
+                    <button className="register-button" type="button" onClick={this.submitregister.bind(this)}>Register</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
