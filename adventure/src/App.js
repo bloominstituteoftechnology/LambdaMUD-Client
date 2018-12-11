2c047ca034ce46798b8477b602423d3c997c7d2e
Csillag61 <csillagok@gmail.com>
2018-12-11 10:45:43 -0500

    global styles in app.js

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import Register from './components/Register';
-import { createGlobalStyle } from 'styled-components';
+import  { createGlobalStyle } from 'styled-components';
 import { connect } from 'react-redux';
 import * as actions from './actions';
 import Login from "./components/Login";
 import Nav from "./components/Nav";
 import Game from "../components/Game";
 
-
+const GlobalStyle = createGlobalStyle`
+  @font-face{
+    font-family: 'radnika_next';
+    src: require(url('/static/radnikanext_medium-webfont.woff2'))
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  } 
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
 
 class App extends Component {
   render() {
