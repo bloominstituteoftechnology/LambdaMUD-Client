9695d8c98b10b82b472a5f8f517ec740375d0602
lyang9 <lyang9@buffalo.edu>
2018-12-11 15:07:14 -0500

    Created class Login

---
+++ lambdamud-client/src/components/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+}
