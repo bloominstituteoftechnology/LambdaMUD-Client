3ceb8625c249d1f912654ae6f6b8dbd82d321ed8
jlaluces123 <jlalucescareer123@gmail.com>
2018-12-11 11:51:49 -0800

    Day 2 - Client: Directional input now works

--- one-hit-hunter/src/components/Mud.js
+++ one-hit-hunter/src/components/Mud.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 class Mud extends React.Component {
   state = {
-    command: ''
+    input: {
+      direction: ''
+    }
   }
   
   initGame = () => {
@@ -26,6 +28,39 @@ class Mud extends React.Component {
 
   } // --> initGame
 
+  makeMove = () => {
+    axios.post('https://one-hit-hunter.herokuapp.com/api/adv/move/', this.state.input, { headers: { Authorization: `Token ${localStorage.getItem('token')}` } })
+      .then(response => {
+        console.log(response)
+        document.getElementById('game-area').value += `\n========================================\n${response.data.title} \n${response.data.description} \n========================================\n`
+        response.data.players.map(player => {
+          console.log(player)
+          if (response.data.players.map.length === 1) {
+            document.getElementById('game-area').value += `No one is standing in this room besides you\n`
+          }
+          document.getElementById('game-area').value += `${player} is standing here\n`
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+
+    const { name, value } = e.target
+    console.log([name],': ', value)
+    this.setState({
+      input : {
+        ...this.state.input,
+        [name]: value
+      }       
+    })
+  }
+
+  handleSubmit = () => {
+    this.makeMove()
+  }
+
   componentDidMount() {
     this.initGame();
   }
@@ -40,7 +75,12 @@ class Mud extends React.Component {
         </div>
         <input
           placeholder = 'User input'
+          name = 'direction'
+          type = 'text'
+          value = {this.state.input.direction}
+          onChange = {this.handleChange}
         />
+        <button onClick={this.handleSubmit}>Send</button>
       </div>
     )
   }
