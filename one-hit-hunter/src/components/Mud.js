18a0fa3439b2391558b92b43cde040e4eb3e3689
jlaluces123 <jlalucescareer123@gmail.com>
2018-12-11 11:13:20 -0800

    Day 2 - Client: Formatted the game textarea to look better

--- one-hit-hunter/src/components/Mud.js
+++ one-hit-hunter/src/components/Mud.js
@@ -2,6 +2,9 @@ import React from 'react'
 import axios from 'axios'
 
 class Mud extends React.Component {
+  state = {
+    command: ''
+  }
   
   initGame = () => {
     // game code here
@@ -10,9 +13,12 @@ class Mud extends React.Component {
     })
       .then(response => {
         console.log(response)
-        document.getElementById('game-area').value += `\n${response.data.title} \n${response.data.description} \n`
+        document.getElementById('game-area').value += `\n========================================\n${response.data.title} \n${response.data.description} \n========================================\n`
         response.data.players.map(player => {
           console.log(player)
+          if (response.data.players.map.length === 0) {
+            document.getElementById('game-area').value += `No one is standing in this room besides you\n`
+          }
           document.getElementById('game-area').value += `${player} is standing here\n`
         })
       })
@@ -28,8 +34,8 @@ class Mud extends React.Component {
     return (
       <div className='game-window'>
         <div className = 'text-area'>
-          <textarea id='game-area' readOnly={true}>
-            Welcome to One Hit Hunter
+          <textarea rows = '30' cols = '100' id='game-area' readOnly={true}>
+            Welcome to One Hit Hunter            
           </textarea>
         </div>
         <input
