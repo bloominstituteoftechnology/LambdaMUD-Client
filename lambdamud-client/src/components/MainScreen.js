9cde809967c972875635eb7b5ed90ea75a146dff
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-11 13:45:29 -0800

    Created move api call. It works. Need to find a way for the direction to be inputed into the input field and cause the player to walk in that direction.

--- lambdamud-client/src/components/MainScreen.js
+++ lambdamud-client/src/components/MainScreen.js
@@ -7,9 +7,19 @@ class MainScreen extends React.Component {
     name: '',
     title: '',
     description: '',
-    players: []
+    players: [],
+    error_msg: '',
   };
   
+  // moveToRoom = event => {
+  //   event.preventDefault();
+
+  //   console.log(this.state);
+
+  //   const userMove = {
+  //     'direction': this.state.direction,
+  //   }   
+
   componentDidMount() {
     const token = localStorage.getItem('token');
 
@@ -19,6 +29,8 @@ class MainScreen extends React.Component {
       }
     }
     
+    const direction = {'direction': 's'}
+
   axios
       .get('https://lambdamud-project-week.herokuapp.com/api/adv/init/', options)
       .then(response => {
@@ -28,8 +40,26 @@ class MainScreen extends React.Component {
       .catch(err => {
         console.log(err.response);
       });
+      
+  axios
+      .post('https://lambdamud-project-week.herokuapp.com/api/adv/move/', direction, options)
+      .then(response => {
+        console.log(response);
+        this.setState({ name: response.data.name, title: response.data.title, description: response.data.description, players: response.data.players, error_msg: response.data.error_msg });
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+    //}
+      // handleInputChange = e => {
+      //   const { name, value } = e.target;
+      //   this.setState({ [name]: value });
+      // }
+
   }
   
+  
+
   render() {
     const playersArray = this.state.players.slice();
     
@@ -43,8 +73,19 @@ class MainScreen extends React.Component {
       {playersArray.map((player,index) => <div className={"players"} key={index} player={player} >
             <p>{player}</p>
           </div>)}
-       
-
+          <p>{this.state.error_msg}</p>
+        <form onSubmit={this.moveToRoom}>
+        <div>
+          <label htmlFor="direction"></label>
+          <input
+            name="direction"
+            value={this.state.direction}
+            onChange={this.handleInputChange}
+            type="text"
+          />
+          <button type="submit">Send</button>
+        </div>
+      </form>
     </div>
     );
   }
