203cd1b64ee533049017e5c23e79e424775f4010
Nicocchi <nicocchilover@gmail.com>
2018-12-11 14:51:15 -0500

    fixed game route upon login

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -58,7 +58,7 @@ function Login(props) {
     let isLogged = props.isLoggedIn;
     if (isLogged) {
         props.getUser();
-        props.history.push('/')
+        props.history.push('/game')
     }
     return (
         <Wrapper>
