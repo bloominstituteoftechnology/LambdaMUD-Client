ef2ff1bcc76e716938c5a554a9ba12123d1b4cc1
Joseph Chretien-Golden <joechre@gmail.com>
2018-12-10 18:09:37 -0600

    front end client side

---
+++ src/components/Login.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import styled from 'styled-components'
+
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.login({
+      "username": this.state.username,
+      "password": this.state.password
+    })
+    this.setState({
+      username: "",
+      password: "",
+    })
+  }
+
+  render() {
+    return (
+      <Form1 onSubmit={this.handleSubmit}>
+        <Label>
+          <Input
+            name="username"
+            type="text"
+            placeholder="👤 username"
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
+        </Label>
+        <Label>
+          <Input
+            name="password"
+            type="password"
+            placeholder="🔑 password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+        </Label>
+        <Button>Login</Button>
+        <P1 onClick={this.props.toggleCreateUserForm}>Create Account</P1>
+      </Form1>
+    )
+  }
+}
+
+const Form1 = styled.form`
+  background: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px black;
+  padding: 1rem;
+`
+const Label = styled.label`
+  margin: 1rem;
+`
+const Input = styled.input`
+  border-radius: 2px;
+  padding: 5px;
+  font-size: 16px;
+  text-align: center;
+  margin: 0 1rem;
+  box-shadow: 0 2px 2px gray;
+  border: none;
+`
+const Button = styled.button`
+  width: 50%;
+  margin: 1rem auto;
+  background: #61DAFB;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 2px;
+  border: none;
+  box-shadow: 0 2px 2px gray;
+  &:hover {
+    cursor: pointer;
+  }
+`
+const P1 = styled.p`
+  text-decoration: underline;
+  cursor: pointer;
+`
+export default Login
---
+++ src/components/Main.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+class Main extends React.Component {
+  state = {
+    title: "",
+    description: "",
+    players: [],
+    uuid: null
+  }
+  componentDidMount() {
+    this.initialize()
+  }
+
+  initialize = () => {
+    const token = localStorage.getItem('token')
+    const headers = { headers: { Authorization: `Token ${token}` } }
+    console.log(token, headers)
+    axios.get('https://lambdamud-timh1203.herokuapp.com/api/adv/init/', headers)
+      .then(res => {
+        this.setState({
+          title: res.data.title,
+          desc: res.data.description,
+          players: res.data.players,
+          uuid: res.data.uuid
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    const { title, desc, players } = this.state
+    return (
+      <Div1>
+        <Div2>
+          <p>Adventure</p>
+          <Div3>
+            <p>{title}</p>
+            <p>{desc}</p>
+            {
+              players.length > 0 && (
+                <p>{players.length === 1 ? `${players} is` : `${players.join(", ")} are`} standing here</p>
+              )
+            }
+          </Div3>
+          <Div4>
+            <p>User input</p>
+            <button onClick={e => this.props.logout(e)}>Send</button>
+          </Div4>
+        </Div2>
+        <button onClick={e => this.props.logout(e)}>Logout</button>
+      </Div1>
+    )
+  }
+}
+const Div1 = styled.div`
+  width: 100%;
+`
+const Div2 = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  border: 1px solid white;
+  padding: 1rem;
+`
+const Div3 = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  border: 1px solid white;
+  padding: 1rem;
+  `
+const Div4 = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  border: 1px solid white;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`
+
+export default Main
