f87144342a69a0f37e6b0fd67978978bead21126
Csillag61 <csillagok@gmail.com>
2018-12-11 11:14:36 -0500

    StyledGame,Fragment

--- adventure/src/components/Game.js
+++ adventure/src/components/Game.js
@@ -1,6 +1,7 @@
-import { Component } from "react";
-import Pusher from "pusher-js";
-import StyledGame, { StyledInput } from "./styles/game";
+
+import React, { Component } from 'react';
+import Pusher from 'pusher-js';
+import StyledGame, { StyledInput } from '../styles/game';
 
 const moves = {
     n: 'n',
@@ -12,47 +13,72 @@ const moves = {
     east: 'e',
     west: 'w'
 };
+
 class Game extends Component {
-    state={
+    state = {
         text: ''
     };
 
-      componentDidMount(){
-          const token = localStorage.getItem('token');
-          this.props.fetchInitInfo(token);
-      }
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+        this.props.fetchInitInfo(token);
+    }
 
-      componentDidUpdate(prevProps){
-          if (this.props.uuid !== prevProps.uuid) {
-              const pusher = new Pusher('',{
-                  cluster:'us2',
-                  encrypted: true
-              });
-              const channel = pusher.subscribe(`p-channel-${this.props.uuid}`);
-              channel.bind('broadcast', data =>
+    componentDidUpdate(prevProps) {
+        if (this.props.uuid !== prevProps.uuid) {
+            const pusher = new Pusher('f5f1c81452944005d664', {
+                cluster: 'us2',
+                encrypted: true
+            });
+            const channel = pusher.subscribe(`p-channel-${this.props.uuid}`);
+            channel.bind('broadcast', data =>
                 this.props.fetchNewMessage(data.message)
-                );
-          }
-      }
+            );
+        }
+    }
 
-      handleChange (event) {
-          this.setState({
-              [event.target.name]: event.target.value
-          });
-          
-      };
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
 
-    handleSubmit(event){
-          event.preventDefault();
-          const token = localStorage.getItem('token');
-          const command = this.state.text.split('');
-          if (moves[command[0]]) {
-              this.props.movePlayer(moves[command[0]], token);
+    handleSubmit = e => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        const command = this.state.text.split(' ');
+        if (moves[command[0]]) {
+            this.props.movePlayer(moves[command[0]], token);
+        } else {
+            this.props.talkPlayer(this.state.text, token);
+        }
+        this.setState({ text: '' });
+    };
 
-          } else{
-              this.props.talkPlayer(this.state.text, token);
-          }
-          this.setState({ text:''});
-      };}
+    render() {
+        return (
+            <React.Fragment>
+                <StyledGame>
+                    <h3>Hello, {this.props.name} </h3>
+                    {this.props.data.map((message, index) => {
+                        return (
+                            <div key={index}>
+                                <p>{message}</p>
+                            </div>
+                        );
+                    })}
+                </StyledGame>
+                <form onSubmit={this.handleSubmit}>
+                    <StyledInput
+                        type="text"
+                        name="text"
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                    />
+                </form>
+            </React.Fragment>
+        );
+    }
+}
 
 export default Game;
