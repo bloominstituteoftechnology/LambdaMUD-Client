109091f91c823e0626e01fa08056e2f84c824eb3
Timothy Hoang <timh1203@gmail.com>
2018-12-12 15:35:51 -0600

    Style text output

--- src/components/MainTextOutput.js
+++ src/components/MainTextOutput.js
@@ -1,5 +1,14 @@
 // Renders out textLog and associated values if exists
 import React from 'react'
+import styled from 'styled-components'
+
+// Styled-Components
+const Span1 = styled.span`
+  text-decoration: underline;
+`
+const Span2 = styled.span`
+  color: lightseagreen;
+`
 
 export default (props) => {
   return (
@@ -8,10 +17,10 @@ export default (props) => {
         props.textLog.map((text, index) => (
           <div key={index}>
             <hr />
-            {text.title && (<p>{text.title}</p>)}
+            <Span1>{text.title && (<p>{text.title}</p>)}</Span1>
             {text.desc && (<p>{text.desc}</p>)}
             {text.players && text.players.length > 0 && (
-              <p>{text.players.length === 1 ? `${text.players} is` : `${text.players.join(", ")} are`} standing here</p>
+              <Span2><p>{text.players.length === 1 ? `${text.players} is` : `${text.players.join(", ")} are`} standing here</p></Span2>
             )}
             {text.message && (<p>{text.message}</p>)}
           </div>
