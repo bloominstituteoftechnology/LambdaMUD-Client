77c2a93b12a2fa67a7c9742d411ebae02b4d271d
lyang9 <lyang9@buffalo.edu>
2018-12-11 19:03:55 -0500

    Imported Login and rendered route path

--- lambdamud-client/src/App.js
+++ lambdamud-client/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 
 import Home from './components/home';
 import Registration from './components/registrations';
+import Login from './components/login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path='/' component={Home} />
-        <Route path='/registration' component={Registration} />
+        <Route path='/register' component={Registration} />
+        <Route path='/login' component={Login} />
       </div>
     );
   }
