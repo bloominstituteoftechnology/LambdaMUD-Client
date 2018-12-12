54d247e84a22e50a4d0da298b0326a45ec1fc54e
Timothy Hoang <timh1203@gmail.com>
2018-12-12 10:43:37 -0600

    Add header comments to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-
-
+// Handles the authentication with axios calls
+// Controls UI before and after logging in
+// Renders signup form and main ui component
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import logo from './logo.svg'
@@ -9,90 +10,7 @@ import Main from './components/Main'
 import axios from 'axios'
 require('dotenv').config()
 
-class App extends Component {
-  state = {
-    creatingUser: false,
-    isLoggedIn: false,
-  }
-
-  componentDidMount() {
-    const token = localStorage.getItem('token')
-    if (token) {
-      this.setState({ isLoggedIn: true })
-    }
-  }
-
-  signup = (newUserObject) => {
-    axios.post(process.env.REACT_APP_SERVER + '/api/registration/', newUserObject)
-      .then(res => {
-        localStorage.setItem('token', res.data.key)
-        this.setState({ isLoggedIn: true })
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    this.toggleCreateUserForm()
-  }
-
-  login = (userObject) => {
-    axios.post(process.env.REACT_APP_SERVER + '/api/login/', userObject)
-      .then(res => {
-        console.log(res)
-        localStorage.setItem('token', res.data.key)
-        this.setState({ isLoggedIn: true })
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
-
-  logout = (e) => {
-    e.preventDefault()
-    localStorage.clear()
-    this.setState({ isLoggedIn: false })
-  }
-
-  toggleCreateUserForm = () => {
-    this.setState({ creatingUser: !this.state.creatingUser })
-  }
-
-  render() {
-    return (
-      <Div1>
-        {
-          !this.state.isLoggedIn && !this.state.creatingUser ? (
-            <>
-              <Img1 src={logo} alt="logo" />
-              <h1>Lambda MUD</h1>
-              <h3>Timothy Hoang</h3>
-              <Login
-                toggleCreateUserForm={this.toggleCreateUserForm}
-                login={this.login}
-              />
-            </>
-          ) : null
-        }
-        {
-          !this.state.isLoggedIn && this.state.creatingUser ? (
-            <Signup
-              toggleCreateUserForm={this.toggleCreateUserForm}
-              signup={this.signup}
-            />
-          ) : null
-        }
-        {
-          this.state.isLoggedIn ? (
-            <Main
-              initialize={this.initialize}
-              logout={this.logout}
-            />
-          ) : null
-        }
-      </Div1>
-    );
-  }
-}
-
+// Styled-Components
 const Div1 = styled.div`
   background-color: #282c34;
   min-height: 100vh;
@@ -114,4 +32,101 @@ const Img1 = styled.img`
     }
   }
 `
-export default App;
+
+export default class App extends Component {
+  state = {
+    isCreatingUser: false,
+    isLoggedIn: false,
+  }
+
+  // FUNCTION: check local storage for token to authenticate user
+  // ARGUMENTS: none
+  // RETURNS: if token present, sets isLoggedIn to true
+  componentDidMount() {
+    const token = localStorage.getItem('token')
+    if (token) {
+      this.setState({ isLoggedIn: true })
+    }
+  }
+
+  // FUNCTION: registers a new user
+  // ARGUMENTS: newUserObject contains new user information from <Signup /> component
+  // RETURNS: sets token in local storage, sets isLoggedIn to true, closes form
+  signup = (newUserObject) => {
+    axios.post(process.env.REACT_APP_SERVER + '/api/registration/', newUserObject)
+      .then(res => {
+        localStorage.setItem('token', res.data.key)
+        this.setState({ isLoggedIn: true })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    this.toggleCreateUserForm()
+  }
+
+  // FUNCTION: logs in a user with existing credentials
+  // ARGUMENTS: userObject packages exisiting information from <Login /> component
+  // RETURNS: sets token in local storage and sets isLoggedIn to true
+  login = (userObject) => {
+    axios.post(process.env.REACT_APP_SERVER + '/api/login/', userObject)
+      .then(res => {
+        localStorage.setItem('token', res.data.key)
+        this.setState({ isLoggedIn: true })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  // FUNCTION: logs out a user
+  // ARGUMENTS: event argument to prevent default action
+  // RETURNS: removes token from local storage, set isLoggedIn to false
+  logout = (e) => {
+    e.preventDefault()
+    localStorage.clear()
+    this.setState({ isLoggedIn: false })
+  }
+
+  // FUNCTION: toggles ui to open/close new user signup form
+  // ARGUMENTS: none
+  // RETURNS: toggles isCreatingUser to true
+  toggleCreateUserForm = () => {
+    this.setState({ isCreatingUser: !this.state.isCreatingUser })
+  }
+
+  render() {
+    return (
+      <Div1>
+        {
+          !this.state.isLoggedIn && !this.state.isCreatingUser ? (
+            <>
+              <Img1 src={logo} alt="logo" />
+              <h1>Lambda MUD</h1>
+              <h3>Timothy Hoang</h3>
+              <Login
+                toggleCreateUserForm={this.toggleCreateUserForm}
+                login={this.login}
+              />
+            </>
+          ) : null
+        }
+        {
+          !this.state.isLoggedIn && this.state.isCreatingUser ? (
+            <Signup
+              toggleCreateUserForm={this.toggleCreateUserForm}
+              signup={this.signup}
+            />
+          ) : null
+        }
+        {
+          this.state.isLoggedIn ? (
+            <Main
+              initialize={this.initialize}
+              logout={this.logout}
+            />
+          ) : null
+        }
+      </Div1>
+    );
+  }
+}
