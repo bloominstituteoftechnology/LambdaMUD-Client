5353062729214f8459feab9b4444cffb44bd4061
Timothy Hoang <timh1203@gmail.com>
2018-12-12 10:51:54 -0600

    Add header comments to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,58 +1,9 @@
+// Form to handle user inputs for logging in
+// Passes user information back to App.js
 import React from 'react'
 import styled from 'styled-components'
 
-class Login extends React.Component {
-  state = {
-    username: "",
-    password: "",
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.login({
-      "username": this.state.username,
-      "password": this.state.password
-    })
-    this.setState({
-      username: "",
-      password: "",
-    })
-  }
-
-  render() {
-    return (
-      <Form1 onSubmit={this.handleSubmit}>
-        <Label>
-          <Input
-            name="username"
-            type="text"
-            placeholder="👤 username"
-            onChange={this.handleChange}
-            value={this.state.username}
-          />
-        </Label>
-        <Label>
-          <Input
-            name="password"
-            type="password"
-            placeholder="🔑 password"
-            onChange={this.handleChange}
-            value={this.state.password}
-          />
-        </Label>
-        <Button>Login</Button>
-        <P1 onClick={this.props.toggleCreateUserForm}>Create Account</P1>
-      </Form1>
-    )
-  }
-}
-
+// Styled-Components
 const Form1 = styled.form`
   background: white;
   display: flex;
@@ -94,4 +45,61 @@ const P1 = styled.p`
   text-decoration: underline;
   cursor: pointer;
 `
-export default Login
+
+export default class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  }
+
+  // FUNCTION: holds user information in state
+  // ARGUMENTS: event argument to prevent default action
+  // RETURNS: updates state with computed property names
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // FUNCTION: passes user login information back to App
+  // ARGUMENTS: event argument to prevent default action
+  // RETURNS: send user information to login method in APP, resets state
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.login({
+      "username": this.state.username,
+      "password": this.state.password
+    })
+    this.setState({
+      username: "",
+      password: "",
+    })
+  }
+
+  render() {
+    return (
+      <Form1 onSubmit={this.handleSubmit}>
+        <Label>
+          <Input
+            name="username"
+            type="text"
+            placeholder="👤 username"
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
+        </Label>
+        <Label>
+          <Input
+            name="password"
+            type="password"
+            placeholder="🔑 password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+        </Label>
+        <Button>Login</Button>
+        <P1 onClick={this.props.toggleCreateUserForm}>Create Account</P1>
+      </Form1>
+    )
+  }
+}
