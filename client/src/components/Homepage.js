4f67105290c70ec8b8397d7962141a181ef8f114
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-12 13:23:54 -0500

    Homepage basic homepage

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  state = {};
+   render() {
+    return (
+      <div>
+        <h1 className="lambda-title">LambdaMUD</h1>
+        
+            <p className= "homepage-login">
+            <p className= "login-text">LOGIN</p>
+            </p>
+        
+      </div>
+    );
+  }
+}
+export default HomePage; 
