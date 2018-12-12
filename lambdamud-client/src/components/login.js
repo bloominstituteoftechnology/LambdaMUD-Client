6b4d92087378ed84497bd37c5f3fe4f004863d3b
lyang9 <lyang9@buffalo.edu>
2018-12-11 19:07:00 -0500

    Created form for login page

--- lambdamud-client/src/components/login.js
+++ lambdamud-client/src/components/login.js
@@ -7,6 +7,36 @@ class Login extends Component {
     this.state = {
       username: '',
       password: '',
-    }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>LambdaMUD</h1>
+        <div>
+          <h2>User Login</h2>
+          <form>
+            <input
+              type='text'
+              name='username'
+              placeholder='Username'
+              value={this.state.username}
+              onChange={this.handleLoginChange}
+            />
+            <input
+              type='password'
+              name='password1'
+              placeholder='Password'
+              value={this.state.password1}
+              onChange={this.handleLoginChange}
+            />
+          </form>
+        </div>
+        <button onClick={this.loginHandler}>Login</button>
+      </div>
+    );
   }
 }
+
+export default Login;
