cf135fd81dafc4eec4c759bba73b303f4ffc5512
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-12 13:46:11 -0500

    RegisterPage non-functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+import {
+  Route, 
+  BrowserRouter as Router, 
+  Switch,
+} from 'react-router-dom'
 import "./App.css";
+import RegisterPage from './components/RegisterPage.js'
 import HomePage from './components/HomePage.js';
 
-
-class App extends Component {
+ class App extends Component {
   render() {
     return (
-      <div className="Contains app">
+      <div className="Contains App">
         <header className="App-header">
-          <HomePage /> 
-          
+          <Router>
+            <Switch>
+              <Route exact path = "/" component={HomePage} />
+              <Route path = "/register" component = {RegisterPage} />
+              <Route component={HomePage} /> 
+              {/* This last line takes the user to the home page in the event they attempt to go to a path that doesn't exist. */}
+            </Switch>
+           </Router> 
         </header>
       </div>
     );
