2cfc2701087ffa68f4620a464d1d48fe6787274b
Anthony Greb <agreb255@gmail.com>
2018-12-12 16:36:49 -0500

    got a simple entry page

--- lambda-client/src/App.js
+++ lambda-client/src/App.js
@@ -2,17 +2,18 @@ import React, {Component} from "react";
 import {Route} from 'react-router-dom';
 import "./App.css";
 import Authenticate from './Authenticate'
+import GamePage from './GamePage'
+import Entry from './Entry'
+import Login from './Login'
 
 class App extends Component {
   render() {
     return <div className = "App" >
-      <Route path = '/api/registration'
-    component = {Authenticate}
-    /> {
-      /* <Route path='/api/login' component={Login} />
-            <Route path='/api/adv/init' component={GamePage} />
-            <Route exact path='/' component={Start} /> */
-    } </div>
+      <Route path = '/api/registration' component = {Authenticate} /> 
+      <Route path='/api/login' component={Login} />
+      <Route path='/api/adv/init' component={GamePage} />
+      <Route exact path='/' component={Entry} /> 
+     </div>
   }
 }
 
