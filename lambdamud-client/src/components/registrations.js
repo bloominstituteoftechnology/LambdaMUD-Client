9113b39d1484fcb23c690e56622bcf8cce429792
lyang9 <lyang9@buffalo.edu>
2018-12-12 17:41:26 -0500

    Modified addUser axios functionality

--- lambdamud-client/src/components/registrations.js
+++ lambdamud-client/src/components/registrations.js
@@ -19,22 +19,79 @@ class Registration extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  // addUser = event => {
+  //   event.preventDefault();
+  //   console.log('STATE: ', this.state);
+  //   // var credentials = {"username": this.state.username, "password1": this.state.password1, "password2": this.state.password2}
+
+  //   axios
+  //     // .post('localhost:8000/api/registration', this.state)
+  //     .post('https://cs13-lambdamudproject.herokuapp.com/api/registration/', this.state)
+  //     .then(res => {
+  //       console.log('RES:', res.data)
+  //       localStorage.setItem('jwt', res.data.key)
+  //       this.props.history.push('/api/adv/init/')
+  //     })
+  //     .catch(err => {
+  //       console.log('Server Error', err)
+  //     });
+  // };
+
   addUser = event => {
     event.preventDefault();
-    console.log('STATE: ', this.state);
-    // var credentials = {"username": this.state.username, "password1": this.state.password1, "password2": this.state.password2}
-
+    const username = this.state.username;
+    const password1 = this.state.password1;
+    const password2 = this.state.password2;
+    // if (password1 !== password2) {
+    //     return 
+    // }
+    const newUser = {
+        username,
+        password1,
+        password2,
+    };
+    console.log(newUser);
+    let data = JSON.stringify({
+        username: this.state.username,
+        password1: this.state.password1,
+        password2: this.state.password2,
+    })
+    console.log(data);
     axios
-      .post('localhost:8000/api/registration', this.state)
-      .then(res => {
-        console.log('RES:', res.data)
-        localStorage.setItem('jwt', res.data.token)
-        this.props.history.push('/')
-      })
-      .catch(err => {
-        console.log('Server Error', err)
-      });
-  };
+        .post('https://cs13-lambdamudproject.herokuapp.com/api/registration/', data, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            withCredentials:false
+        })
+        .then(response => {
+            console.log(response);
+            localStorage.setItem('token', response.data.key);
+            this.props.history.push('/api/adv/init')
+        })
+        .catch(err => console.log(err))
+
+    this.setState({
+        username: '',
+        password1: '',
+        password2: '',
+    });
+}
+
+// addUser = e => {
+//   // e.preventDefault();
+//   const userInfo = { username: this.state.username, password1: this.state.password1, password2: this.state.password2 };
+//   console.log('userInfo: ', userInfo)
+
+//   axios
+//       .post('https://cs13-lambdamudproject.herokuapp.com/api/registration/', userInfo)
+//       .then(r => {
+//           console.log('r: ', r)
+//           localStorage.setItem('token', r.data.key);
+//           this.props.history.push('/api/adv/init')
+//       })
+//       .catch(err => console.log(err.response))
+// }
 
   render() {
     console.log(this.state);
