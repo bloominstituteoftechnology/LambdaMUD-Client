4af88a74ca687965ff9ad43167976a52cc498662
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-12 17:11:29 -0800

    Move and Say endpoints work now. userSignOut still does not work.

--- lambdamud-client/src/components/MainScreen.js
+++ lambdamud-client/src/components/MainScreen.js
@@ -15,7 +15,7 @@ class MainScreen extends React.Component {
     description: '',
     players: [],
     error_msg: '',
-    direction: '',
+    userInput: '',
   };
 
   
@@ -63,7 +63,7 @@ class MainScreen extends React.Component {
     }
     
         const userMove = {
-          'direction': this.state.direction,
+          'direction': this.state.userInput,
         }       
 
         axios
@@ -80,6 +80,36 @@ class MainScreen extends React.Component {
         this.props.history.push('/mainscreen');
       }
 
+    sayInRoom = event => {
+      event.preventDefault();
+
+      const token = localStorage.getItem('token');
+
+      const options = {
+          headers: {
+            "Authorization": 'Token ' + token
+        }
+
+  }
+  
+      const userSay = {
+        'message': this.state.userInput,
+      }       
+  
+      axios
+      .post('https://lambdamud-project-week.herokuapp.com/api/adv/say/', userSay, options)
+      .then(response => {
+        console.log(response);
+        this.setState({ name: response.data.name, title: response.data.title, description: response.data.description, players: response.data.players, error_msg: response.data.error_msg });
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+
+      this.setState({description: ''});
+      this.props.history.push('/mainscreen');
+    }
+
       handleInputChange = e => {
         const { name, value } = e.target;
         this.setState({ [name]: value });
@@ -88,6 +118,37 @@ class MainScreen extends React.Component {
 
   render() {
     const playersArray = this.state.players.slice();
+
+    if (this.state.userInput === "n" || this.state.userInput == "e" || this.state.userInput == "w" || this.state.userInput == "s" ) {
+    return(
+      <div>
+        <div className='adv-title-div' >
+        <h1>Adventure</h1>
+        </div>
+          <p>{this.state.name}, you are in the</p>
+          <p>{this.state.title}</p>
+          <p>{this.state.description}</p>
+        
+        {playersArray.map((player,index) => <div className={"players"} key={index} player={player} >
+              <p>{player} is standing here</p>
+            </div>)}
+            <p>{this.state.error_msg}</p>
+  
+          <form className='mainscreen-adv' onSubmit={this.moveToRoom}>
+          <div className='mainscreen-form-div'>
+            <label htmlFor="userInput"></label>
+            <input
+              name="userInput"
+              value={this.state.userInput}
+              onChange={this.handleInputChange}
+              type="text"
+            />
+            <button type="submit">Send</button>
+          </div>
+        </form>
+      </div>
+      );
+    } 
     
     return(
     <div>
@@ -102,12 +163,13 @@ class MainScreen extends React.Component {
             <p>{player} is standing here</p>
           </div>)}
           <p>{this.state.error_msg}</p>
-        <form className='mainscreen-adv' onSubmit={this.moveToRoom}>
+
+        <form className='mainscreen-adv' onSubmit={this.sayInRoom}>
         <div className='mainscreen-form-div'>
-          <label htmlFor="direction"></label>
+          <label htmlFor="userInput"></label>
           <input
-            name="direction"
-            value={this.state.direction}
+            name="userInput"
+            value={this.state.userInput}
             onChange={this.handleInputChange}
             type="text"
           />
@@ -117,7 +179,6 @@ class MainScreen extends React.Component {
     </div>
     );
   }
-
 }
 
 export default MainScreen;
