9ee650b216a176a64c435ad4e2f59f77ccf13f81
lyang9 <lyang9@buffalo.edu>
2018-12-13 12:07:22 -0500

    Imported play and rendered route path

--- lambdamud-client/src/App.js
+++ lambdamud-client/src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import Home from './components/home';
 import Registration from './components/registrations';
 import Login from './components/login';
+import Play from './components/play';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/register' component={Registration} />
         <Route path='/login' component={Login} />
+        <Route path='/api/adv/init' component={Play} />
       </div>
     );
   }
