ea1903f1581200aa6468132b7799310d5d63ece0
Brandon White <bwhite06@stac.edu>
2018-12-16 14:35:46 -0500

    updated pusher

--- lambda-mud-frontend/src/Components/MainScreen/MainScreen.js
+++ lambda-mud-frontend/src/Components/MainScreen/MainScreen.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import "./MainScreenCss.css";
-import {NavLink} from "react-router-dom";
+import Pusher from 'pusher-js';
+import Players from "./Players"
+
+
+ 
 
 class MainScreen extends Component {
 
@@ -10,10 +14,27 @@ class MainScreen extends Component {
         name: "", 
         title: "",
         description: "", 
-        players: []
+        players: [],
+        message:"",
+        chatMessage:"",
+        chats:[]
+        
     }
 
 componentDidMount(){
+
+    this.pusher = new Pusher('d6c84f9ec64fbcd9df76', {
+        cluster: 'us2',
+        forceTLS: true,
+        authEndpoint: '/pusher/auth',
+      });
+    //access channel for player room
+   this.channel = this.pusher.subscribe('p-channel-ec5ea3bc-3002-4c6a-9ab7-3e763855a94e');
+   this.channel.bind('broadcast', data => {
+
+        this.setState({ chats: [...this.state.chats, data.message]});
+      },this);
+
    const api = 'https://lambda-mud-backend.herokuapp.com/api/adv/init/';
    const token = window.sessionStorage.getItem('authKey');
      console.log(token)
@@ -33,6 +54,30 @@ componentDidMount(){
     })
 
     }
+
+    sendMes = (event) => {
+        event.preventDefault()
+       
+
+       const message = {message:this.state.message};
+       const URL = 'https://lambda-mud-backend.herokuapp.com/api/adv/say/';
+       const token = window.sessionStorage.getItem('authKey');
+        axios
+        .post(URL, message , { headers: {Authorization: `Token ${token}`}})
+        .then(response => {
+            console.log(response.data.message)
+            this.setState({
+            chatMessage: response.data.message,
+            });
+
+        },this.props.history.push(`/MainScreen`))
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+
+
+
 
     handleInputChange = event => {
         this.setState({ [event.target.name]: event.target.value });
@@ -63,19 +108,30 @@ componentDidMount(){
 
 
 render(){
+   
     return(
 <div className= 'mainScreen'>
 <div className = 'gameinfo'>
 <h3>Username: {this.state.name}</h3>
-<h3> Players in Room: {this.state.players}</h3>
+<div> Players in Room: {this.state.players.map(player => (
+          <Players key={player.id} player={player} />
+        ))}
+</div>
 <h1>{this.state.title}</h1>
 <hr/>
 <p>{this.state.description}</p>{}
 </div>
 <input className = "userInput" placeholder='userInput' name= 'userInput' onChange ={this.handleInputChange} value = {this.state.userInput}/>
-
+<p>Enter direction:</p>
 <button className = "submit" onClick={this.move}>submit</button>
+<hr/>
+<h3>Room chatboard:</h3>
+<div>message:{this.state.chats.map(player => (
+          <Players key={player.id} player={player} />
+        ))}</div>
 
+<input className = "message" placeholder='message' name= 'message' onChange ={this.handleInputChange} value = {this.state.message}/>
+<button onClick={this.sendMes}>Send</button>
 </div>
 
     )
@@ -86,3 +142,5 @@ render(){
 }
 
 export default MainScreen
+
+
---
+++ lambda-mud-frontend/src/Components/MainScreen/Players.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Players extends Component {
+
+constructor(props){
+    super(props)
+
+}
+
+
+
+render(){
+return <h3>{this.props.player}</h3>
+}
+
+
+
+}
+export default Players 
