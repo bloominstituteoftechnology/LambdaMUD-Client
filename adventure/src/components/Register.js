4a3e5b976050ef1e0ea8e38465cba17eab6d5122
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-18 19:01:23 -0500

    register page finished

--- adventure/src/components/Register.js
+++ adventure/src/components/Register.js
@@ -47,7 +47,7 @@ class Register extends React.Component {
     return (
       <Form className="GameStart">
         <FormGroup>
-          <h2>Register Below</h2>
+          <h2 className="login-text">Create an Account</h2>
           <Label for="exampleEmail">Username</Label>
           <Input 
             type="text"
@@ -78,7 +78,7 @@ class Register extends React.Component {
           />
         </FormGroup><br />
         <Card>
-          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          <CardText>By clicking "Accept the ToS" you are agreeing to have an awesome adventure in this MUD based game!
           </CardText>
         </Card><br />
             <Label check>
@@ -86,9 +86,11 @@ class Register extends React.Component {
                 Accept the ToS!
               </Label>
             <br /><br />
-            <Button color="primary" onClick={this.submitHandler}>Register</Button>
+            <button class="login-button" onClick={this.submitHandler}>Register</button>
             <br /><br />
-            <Link to="/api/login">Already a member?</Link>
+            <Link to="/api/login">
+          <button type="button" className="login-button">Already a member?</button>
+        </Link>
       </Form>
     )
   }
