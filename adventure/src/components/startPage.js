a912fdf687a07bc1eb27c60353c316f153992188
skennedy93 <sean47900@gmail.com>
2019-02-13 14:31:51 -0600

    trying to deploy to netlify

---
+++ adventure/src/components/startPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+class startPage extends Component{
+
+    render() {
+        return(
+            <div className = 'welcomeBox'>
+            <div className = 'intro'>Welcome To The Adventure Game!</div>
+            <Link to = {`/login`}>
+                <button>Continue Adventure</button>
+            </Link>
+
+            <Link to = {`/registration`}>
+                <button>Create Player</button>
+            </Link>
+            </div>
+        )
+    }
+}
+
+export default startPage;
