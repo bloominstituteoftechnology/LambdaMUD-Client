6c596d99b7acb1f61cbf3c9d8ba26e9aaebed14e
Michael Littleton <michaelglittleton@gmail.com>
2019-07-10 15:20:16 -0400

    Got initial data from server and disp basic info

--- mud-py/src/App.js
+++ mud-py/src/App.js
@@ -6,32 +6,54 @@ import Dungeon from './Components/Dungeon';
 import RoomInfo from './Components/RoomInfo';
 import Commands from './Components/Commands';
 import { CssBaseline, Container } from '@material-ui/core';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      currentRoom: {}
     };
+
+    this.content = {
+      headers: {
+        Authorization: 'Token 626a1a9d5ab38fe08f0bab5d5b75f13fb36a12d0'
+      }
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://lambda-mud-test.herokuapp.com/api/adv/init/', this.content)
+      .then(data => {
+        this.setState({currentRoom: data.data})
+
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   tempChangeLogin = () => {
     this.setState(prev => {
-      return {loggedIn: !prev.loggedIn}
-    })
-  }
+      return { loggedIn: !prev.loggedIn };
+    });
+  };
 
   render() {
     return (
       <div>
         <CssBaseline />
         <NavBar tempChangeLogin={this.tempChangeLogin} />
-        {!this.state.loggedIn ? <Login /> : (
+        {!this.state.loggedIn ? (
+          <Login />
+        ) : (
           <Container>
             <Dungeon />
             <ChatBox />
             <Commands />
-            <RoomInfo />
+            <RoomInfo currentRoom={this.state.currentRoom} />
           </Container>
         )}
       </div>
