d1ebcbcbdc1e093afa86dea27b1db9277b9d0cc9
Michael Littleton <michaelglittleton@gmail.com>
2019-07-11 22:59:04 -0400

    Starting to come together

--- mud-py/src/Components/RoomInfo.js
+++ mud-py/src/Components/RoomInfo.js
@@ -1,20 +1,45 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 
 function RoomInfo(props) {
   return (
-    <div>
-      {props.currentRoom.title} <br />
-      {props.currentRoom.description}
-      {props.currentRoom.players ? (
-        <div>
-          Players who are in the room with you:{' '}
-          {props.currentRoom.players.map((player, index) => {
-            return <div key={index}>{player}</div>;
-          })}
-        </div>
-      ) : (
-        <div>There are no players in sight</div>
-      )}
+    <div
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-around"
+      style={{
+        backgroundColor: 'black',
+        color: 'white',
+        width: '30%',
+        height: '30vh',
+        paddingLeft: '15px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+      }}
+    >
+      <div>{props.currentRoom.title} </div>
+      <div>{props.currentRoom.description}</div>
+      <div
+        style={{
+          backgroundColor: 'black',
+          color: 'white',
+          paddingLeft: '15px',
+          display: 'flex',
+          justifyContent: 'space-around'
+        }}
+      >
+        {props.currentRoom.players ? (
+          <div>
+            Players who are in the room with you:{' '}
+            {props.currentRoom.players.map((player, index) => {
+              return <div key={index}>{player}</div>;
+            })}
+          </div>
+        ) : (
+          <div>There are no players in sight</div>
+        )}
+      </div>
     </div>
   );
 }
