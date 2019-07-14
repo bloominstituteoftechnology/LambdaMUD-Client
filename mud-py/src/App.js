6c176155834addbec161bc1ba69fcad1a455545b
Michael Littleton <michaelglittleton@gmail.com>
2019-07-14 11:58:10 -0400

    Changed Routes to accept login and dungeon
    Dungeon page is now separate comonent with subcomponents
    
    Login/register page now push to the dungeon on successful login/register

--- mud-py/src/App.js
+++ mud-py/src/App.js
@@ -1,21 +1,18 @@
-import React from "react";
-import { withRouter } from "react-router";
-import { Route } from "react-router-dom";
-import Landing from "./Components/Landing";
-import NavBar from "./Components/NavBar";
-import Login from "./Components/Login";
-import About from "./Components/About";
-import ChatBox from "./Components/ChatBox";
-import Dungeon from "./Components/Dungeon";
-import RoomInfo from "./Components/RoomInfo";
-import Commands from "./Components/Commands";
-import { CssBaseline, Container } from "@material-ui/core";
-import axios from "axios";
-import "./App.css";
+import React from 'react';
+import { withRouter } from 'react-router';
+import { Route } from 'react-router-dom';
+import Landing from './Components/Landing';
+import NavBar from './Components/NavBar';
+import Login from './Components/Login';
+import About from './Components/About';
+import DungeonPage from './Components/DungeonPage';
+import { CssBaseline } from '@material-ui/core';
+import axios from 'axios';
+import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       loggedIn: false,
       currentRoom: {}
@@ -23,14 +20,14 @@ class App extends React.Component {
 
     this.content = {
       headers: {
-        Authorization: ""
+        Authorization: ''
       }
     };
   }
 
   componentDidMount() {
     const token = localStorage.getItem('Authorization');
-    this.content.headers.Authorization = "Token " + token
+    this.content.headers.Authorization = 'Token ' + token;
     if (token) {
       this.setState({ loggedIn: true });
     } else {
@@ -40,7 +37,7 @@ class App extends React.Component {
 
   componentDidUpdate() {
     axios
-      .get("https://lambda-mud-test.herokuapp.com/api/adv/init/", this.content)
+      .get('https://lambda-mud-test.herokuapp.com/api/adv/init/', this.content)
       .then(data => {
         this.setState({ currentRoom: data.data });
       })
@@ -56,14 +53,16 @@ class App extends React.Component {
   };
 
   login = () => {
-    if (localStorage.getItem("Authorization")) {
-      this.setState({loggedIn: true})
+    if (localStorage.getItem('Authorization')) {
+      this.setState({ loggedIn: true });
+      this.props.history.push('/dungeon')
     }
-  }
+  };
 
   logout = () => {
     localStorage.removeItem('Authorization');
-    this.setState({loggedIn: false})
+    this.setState({ loggedIn: false });
+    this.props.history.push('/login')
   };
 
   render() {
@@ -73,17 +72,8 @@ class App extends React.Component {
         <NavBar tempChangeLogin={this.tempChangeLogin} />
         <Route exact path="/" component={Landing} />
         <Route path="/about" component={About} />
-        <Route exact path="/login" component={Login} />
-        {/* {!this.state.loggedIn ? (
-          <Login />
-        ) : (
-          <Container>
-            <Dungeon />
-            <ChatBox />
-            <Commands />
-            <RoomInfo logout={this.logout} currentRoom={this.state.currentRoom} />
-          </Container>
-        )} */}
+        <Route path="/login" render={() => <Login login={this.login} /> }  />
+        <Route path="/dungeon" render={() => <DungeonPage state={this.state} />} />
       </div>
     );
   }
