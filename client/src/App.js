94b71b2d935976410ca0caa2629f422c5d0f5fd0
sriley <sarahriley.dev@gmail.com>
2019-07-30 14:56:59 -0400

    refactor login/register together, remove header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+
 import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from the APP!!</h1>
+      
       <header className="App-header">
         <Switch>
-        <Route exact path="/" component={Login} />
+         
+        <Route exact path="/login" component={Login} />
         </Switch>
       </header>
     </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,40 +1,79 @@
 import React, { Component } from "react";
-import { AUTH_TOKEN } from "../constants";
+import axios from "axios";
 
 class Login extends Component {
   state = {
     login: true, // switch between Login and SignUp
-    email: "",
+    username: "",
     password: "",
-    name: ""
+    password2: ""
+  };
+
+  handleInput = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  register = (username, password, password2) => {
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/registration/", {
+        username: username,
+        password1: password,
+        password2: password2
+      })
+      .then(response => {
+        localStorage.setItem("key", response.data.key);
+        this.props.history.push("/init");
+      })
+      .catch(error => {
+        alert(error.response.data.error);
+      });
+  };
+
+  login = (username, password) => {
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/registration/", {
+        username: username,
+        password: password
+      })
+      .then(response => {
+        localStorage.setItem("key", response.data.key);
+        this.props.history.push("/init");
+      })
+      .catch(error => {
+        console.log(error.response);
+        alert(error.response.data.error);
+      });
   };
 
   render() {
-    const { login, email, password, name } = this.state;
+    const { login, username, password, password2 } = this.state;
     return (
       <div>
         <h4 className="toggle">{login ? "Login" : "Sign Up"}</h4>
         <div className="column">
-          {!login && (
-            <input
-              value={name}
-              onChange={e => this.setState({ name: e.target.value })}
-              type="text"
-              placeholder="Your name"
-            />
-          )}
           <input
-            value={email}
-            onChange={e => this.setState({ email: e.target.value })}
+            value={username}
+            onChange={e => this.setState({ username: e.target.value })}
             type="text"
-            placeholder="Your email address"
+            placeholder="Your username"
           />
+
           <input
             value={password}
             onChange={e => this.setState({ password: e.target.value })}
             type="password"
-            placeholder="Choose a safe password"
+            placeholder="Your password"
           />
+          {!login && (
+            <input
+              value={password2}
+              onChange={e => this.setState({ password2: e.target.value })}
+              type="password"
+              placeholder="Retype your password"
+            />
+          )}
         </div>
         <div className="div">
           <div className="pointer" onClick={() => this._confirm()}>
@@ -51,15 +90,7 @@ class Login extends Component {
     );
   }
 
-  _confirm = async () => {
-    // const { token } = this.state.login ? data.login : data.signup;
-    // this._saveUserData(token);
-    // this.props.history.push(`/`);
-  };
 
-  _saveUserData = token => {
-    localStorage.setItem(AUTH_TOKEN, token);
-  };
 }
 
 export default Login;
