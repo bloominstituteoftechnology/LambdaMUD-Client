370a381c87639ecb3cff767b9b8d16e50837dd03
chef27615 <wei.du.2015@marshall.usc.edu>
2019-07-30 16:30:33 -0400

    recode

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import Login from './component/login/Login'
-import Header from './component/header/Header'
-import Footer from './component/footer/Footer'
+
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <>
-        <Header />
-        <h1>Hello from the APP!!</h1>
-        <header className="App-header">
-          <Switch>
-          <Route exact path="/login" component={Login} />
-          <Login/>
-          </Switch>
-        </header>
-        <Footer />
-      </>
+      
+      <header className="App-header">
+        <Switch>
+        <Route exact path="/login" component={Login} />
+        </Switch>
+      </header>
     </div>
   );
 }
