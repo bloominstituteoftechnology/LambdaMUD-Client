9b2934e2388c5a508b3cbcf22b18b02d9ee5c7f7
chef27615 <wei.du.2015@marshall.usc.edu>
2019-07-30 10:44:52 -0400

    re-initiated front

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {
+    NavLink,
+    Link,
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+import Home from './component/Home'
+import Game from './component/Game'
+import Signup from './component/Signup'
+import Login from './component/Login'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({ uri: 'http://localhost:4000'})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(
+<ApolloProvider client = {client}>
+    <Router>
+        <>
+            <nav className='navBar'>
+                
+                <NavLink to='/'>Home</NavLink>               
+                <NavLink to='/game'>Game</NavLink>  
+                <NavLink to='/signup'>Signup</NavLink>
+                <NavLink to='/login'>Login</NavLink>
+            </nav>
+            <div className='linkBar'>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/game' component={Game} />
+                    <Route path='/signup' component={Signup} />
+                    <Route path='/login' component={Login} />
+                </Switch>
+            </div>
+        </>
+    </Router>
+</ApolloProvider>, document.getElementById('root'));
+
