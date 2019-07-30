3f404371dc528005d6f7f0a463e98ab13bd9dc34
chef27615 <wei.du.2015@marshall.usc.edu>
2019-07-30 18:38:33 -0400

    add checkAuth and PriivateRoute fn in App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Login from './components/Login'
-import Room from './components/Room';
 import Init from './components/Init';
+
+const checkAuth = () => {
+  const token = localStorage.getItem('token');
+  if(!token){ return false }
+  return true
+}
+
+const PrivateRoute =({ component: Component, ...rest }) => {
+  <Route {...rest} render={props => (
+    checkAuth() ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to={{ pathname:'/login' }} />
+    ) 
+  )} />
+}
+
 
 function App() {
   return (
@@ -12,7 +28,7 @@ function App() {
       <header className="App-header">
         <Switch>
           <Route exact path="/login" component={Login} />
-          <Route exact path="/init" component={Init} />
+          <PrivateRoute exact path="/init" component={Init} />
         </Switch>
       </header>
     </div>
