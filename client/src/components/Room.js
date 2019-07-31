2315318098698343c46856bce847b45f52dd0d2b
sriley <sarahriley.dev@gmail.com>
2019-07-31 11:47:33 -0400

    added token to axios call

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,47 +1,74 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-import RoomDetail from './Room_detail'
+import RoomDetail from "./Room_detail";
 
 export default class Room extends Component {
-    state = {
-        room: []
-    }
+  state = {
+    room: [],
+    key: localStorage.getItem("key")
+  };
 
-    componentDidMount(){
-        axios
-            .get('http://lambda-mud-test.herokuapp.com/api/adv/rooms/') //waiting for endpoint
-            .then(res => {
-                this.setState({
-                    room: res.data
-                })
-            })
-            .catch(e => {
-                console.log(e)
-            })
+  componentDidMount() {
+    if (localStorage.getItem("key")) {
+      this.getRooms();
     }
+  }
 
-    render() {
-        console.log('state', this.state.room)
-        
-        const {room} = this.state
-        let objValues = Object.values(room)
-            
+  handleInput = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
 
-        let b = JSON.stringify(objValues)
-        let c = JSON.parse(b)
-        console.log(c)
-        const { model, pk, fields } = c
-        console.log('object', objValues) 
-        console.log('model', model)
-        console.log('pk', pk)
-        console.log('fields', fields)
-        return (
-            <div>
-                {this.state.room.map(room => 
-                    <RoomDetail room={room} key={room.title}/>
-                )}
-            </div>
-        )
-    }
+  getRooms = () => {
+    const header = {
+      Authorization: `Token ${this.state.key}`
+    };
+    axios
+      .get("http://lambda-mud-test.herokuapp.com/api/adv/rooms/", {
+        headers: header
+      })
+      .then(response => {
+        this.setState({ room: response.data });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  };
+  // axios
+  // .get('http://lambda-mud-test.herokuapp.com/api/adv/rooms/') //waiting for endpoint
+  // .then(res => {
+  //     this.setState({
+  //         room: res.data
+  //     })
+  // })
+  // .catch(e => {
+  //     console.log(e)
+  // })
+
+  render() {
+    console.log("state", this.state.room);
+
+    const {room} = this.state
+    let objValues = Object.values(room)
+
+    let b = JSON.stringify(objValues)
+    let c = JSON.parse(b)
+    console.log(c)
+    const { model, pk, fields } = c
+    console.log('object', objValues)
+    console.log('model', model)
+    console.log('pk', pk)
+    console.log('fields', fields)
+    return (
+      <div>
+        {this.state.room
+          ? this.state.room.map(room => (
+              <RoomDetail room={room} key={room.title} />
+            ))
+          : null}
+      </div>
+    );
+  }
 }
