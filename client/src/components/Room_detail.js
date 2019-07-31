120871e62b7c100009e5fb502e38a1c46f0fb474
chef27615 <wei.du.2015@marshall.usc.edu>
2019-07-31 15:49:16 -0400

    props rendering in room_detail file

--- client/src/components/Room_detail.js
+++ client/src/components/Room_detail.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function Room_detail(props) {
+    console.log('in detail', props)
     return (
-        <div className='roomDetail'>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
+        <div className='roomDetail' >
+            <h2>{props.room.title}</h2>
+            <p>{props.room.description}</p>
         </div>
     )
 }
