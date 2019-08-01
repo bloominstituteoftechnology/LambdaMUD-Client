c411853f291dac773ba69e6c29bd8110d633bcb0
chef27615 <wei.du.2015@marshall.usc.edu>
2019-08-01 11:48:33 -0400

    commit before pull

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,6 +4,35 @@ export default class Map extends Component {
     state={
         div:[]    
     }
+
+    mapping = () => {
+        const max_x = 100;
+        const max_y = 100;
+        let x = 0;
+        let y = 0;
+        let cor = { x , y }
+        for(let i=0; i< max_x; i++){
+            for(let j=0; j<max_y; j++){
+                x+=1
+                y+=1
+                return cor 
+            }
+        }
+
+    }
+
+    getRoom = () => {
+        axios
+        .get('__link__here__')
+        .then(res => {
+            //get room information here
+            if(room.s_to||room.n_to||room.e_to||room.w_to > 0){
+                div += 
+            }
+        })
+    }
+
+
     renderDiv = (room) => {
         if(room){
             return div render
