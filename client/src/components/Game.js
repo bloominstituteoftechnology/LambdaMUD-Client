e74def12dabd0daff4c28be8808082a52ec40d14
sriley <sarahriley.dev@gmail.com>
2019-08-01 18:30:54 -0400

    refactor init file and rooms into game file

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
+
+import Map from "./Map";
+import RoomDetail from "./Room_detail";
+
 export default class Init extends Component {
   state = {
     uuid: "",
@@ -8,6 +12,7 @@ export default class Init extends Component {
     description: "",
     direction: "",
     players: [],
+    room: [],
     key: localStorage.getItem("key")
   };
   componentDidMount() {
@@ -26,10 +31,10 @@ export default class Init extends Component {
     const header = {
       Authorization: `Token ${this.state.key}`
     };
-    console.log("HEADER", header)
+    console.log("HEADER", header);
     axios
-      .get("http://lambda-mud-test.herokuapp.com/api/adv/init/", {
-          headers: header
+      .get("https://f-troop-adventures.herokuapp.com/api/adv/init/", {
+        headers: header
       })
       .then(res => {
         this.setState({
@@ -42,11 +47,27 @@ export default class Init extends Component {
       });
   };
 
+  handleMove = e => {
+    const header = {
+      Authorization: `Token ${this.state.key}`
+    };
+    axios
+      .post("https://f-troop-adventures.herokuapp.com/api/adv/move/", {
+        direction: "n",
+        headers: header
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  };
   render() {
     const { uuid, name, title, description, players } = this.state;
     return (
       <div>
-        <h3>Your information</h3>
+        <h3>Your information:</h3>
         <p>{uuid}</p>
         <p>Your name: {name}</p>
         <p>you are here at {title}</p>
@@ -54,6 +75,15 @@ export default class Init extends Component {
         <p>
           other player in the same area as you: {players.map(player => player)}
         </p>
+        <Map />
+        {this.state.room
+          ? this.state.room.map(room => (
+              <RoomDetail room={room} key={room.title} />
+            ))
+          : console.log("no keys")}
+        <button onClick={this.handleMove} name="n">
+          btn
+        </button>
       </div>
     );
   }
