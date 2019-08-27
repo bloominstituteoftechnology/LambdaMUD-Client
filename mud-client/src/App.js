4ba5618cf2a5e1d52d5cf8b6a4abddad8c22c14e
jkaddu <jkaddu@yahoo.com>
2019-08-27 13:12:42 -0700

    basic set up of game

--- mud-client/src/App.js
+++ mud-client/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from './components/Login';
+import Register from './components/Register';
+import Game from './components/Game';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Route exact path="/" render={(props) => <Login {...props} login={this.handleChange} />} />
+			<Route path="/register" render={(props) => <Register {...props} register={this.handleChange} />} />
+			<Route path="/game" component={Game} />
+		</div>
+	);
 }
 
 export default App;
